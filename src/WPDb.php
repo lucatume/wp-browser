e3a0ec52d5302833be7d4df65b56590f26e43a54
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-06-30 13:00:29 +0200

    created the WPDb class as an extension to the Codeception\Module\Db class

--- src/WPDb.php
+++ src/WPDb.php
@@ -2,7 +2,26 @@
 
 namespace Codeception\Module;
 
+use Codeception\Module\Db;
 
-class WPDb {
+class WPDb extends Db
+{
+    protected $config = array('tablePrefix' => 'wp');
 
-} 
+    public function _initialize()
+    {
+        parent::_initialize();
+        $this->tablePrefix = $this->config['tablePrefix'];
+    }
+
+    public function haveOptionInDatabase($option_name, $option_value)
+    {
+        $tableName = $this->config['tablePrefix'] . '_options';
+        $this->db->haveInDatabase($tableName, array($ption_name => $option_value));
+    }
+
+    public function haveSerializedOptionInDatabase($option_name, $option_value)
+    {
+        $this->haveOptionInDatabase($option_name, @serialize($option_value));
+    }
+}
