a46a5a22780df6ec919ecb669fc19606f6ccc1ab
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-07-01 16:54:51 +0200

    added the haveUserInDatabase and seeUserInDatabase methods to the WPDb module

--- src/WPDb.php
+++ src/WPDb.php
@@ -2,7 +2,10 @@
 
 namespace Codeception\Module;
 
+require dirname(__FILE__) . '/UserMaker.php';
+
 use Codeception\Module\Db;
+use tad\test\wordpress\generator\UserMaker;
 
 class WPDb extends Db
 {
@@ -12,6 +15,32 @@ class WPDb extends Db
     {
         parent::_initialize();
         $this->tablePrefix = $this->config['tablePrefix'];
+    }
+
+    public function haveUserInDatabase($user_login, $user_id, $role = 'subscriber', Array $userData = [])
+    {
+        if (!is_string($user_login)) {
+            throw new \BadMethodCallException('User login name must be a string', 1);
+        }
+        $userTableDefaults = UserMaker::generateUserDefaultsFrom($user_login, $user_id, $role);
+        $userCapabilitiesDefaults = UserMaker::generateCapabilitiesDefaultsFrom($user_id, $role);
+        $userLevelDefaults = UserMaker::generateUserLevelDefaultsFrom($user_id, $role);
+        // merge user data with defaults
+        $userTableData = array_merge($userTableDefaults, $userData);
+        $userCapabilitiesData = array_merge($userCapabilitiesDefaults, $userData);
+        $userLevelData = array_merge($userLevelDefaults, $userData);
+        // add the data to the database
+        $tableName = $this->getPrefixedTableNameFor('users');
+        $this->haveInDatabase($tableName, $userTableData);
+        $tableName = $this->getPrefixedTableNameFor('usermeta');
+        $this->haveInDatabase($tableName, $userCapabilitiesData);
+        $this->haveInDatabase($tableName, $userLevelDefaults);
+    }
+
+    public function seeUserInDatabase(Array $criteria)
+    {
+        $tableName = $this->getPrefixedTableNameFor('users');
+        $this->seeInDatabase($tableName, $criteria);
     }
 
     public function haveOptionInDatabase($option_name, $option_value)
