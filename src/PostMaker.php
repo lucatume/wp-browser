f94efd59d2619ffeae817791cf3cf01216509cc1
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-07-03 14:42:41 +0200

    moved the logic for user generation to the UserMaker class

--- src/PostMaker.php
+++ src/PostMaker.php
@@ -2,7 +2,41 @@
 
 namespace tad\wordpress\maker;
 
+use Badcow\LoremIpsum\Generator;
+use tad\utils\Str;
 
-class PostMaker {
-
-} 
+class PostMaker
+{
+    public static function makePost($ID, $url = 'http://www.example.com', Array $data = array())
+    {
+        $loremMaker = new Generator();
+        // create a default value array
+        $title = implode('', $loremMaker->getSentences(1));
+        $content = implode("\n", $loremMaker->getParagraphs(3));
+        $guid = rtrim($url);
+        $defaults = array(
+            'ID' => $ID,
+            'post_author' => 1,
+            'post_date' => DateMaker::now(),
+            'post_date_gmt' => DateMaker::gmnow(),
+            'post_content' => $content,
+            'post_title' => $title,
+            'post_excerpt' => '',
+            'post_status' => 'publish',
+            'comment_status' => 'open',
+            'ping_status' => 'open',
+            'post_password' => '',
+            'post_name' => Str::hyphen($title),
+            'to_ping' => '',
+            'pinged' => '',
+            'post_modified' => DateMaker::now(),
+            'post_modified_gmt' => DateMaker::gmnow(),
+            'post_content_filtered' => '',
+            'post_parent' => 0,
+            'guid' => $guid,
+            'menu_order' => 0,
+            'post_type' => 'post'
+        );
+        return array_merge($defaults, $data);
+    }
+}
--- src/UserMaker.php
+++ src/UserMaker.php
@@ -2,43 +2,60 @@
 
 namespace tad\wordpress\maker;
 
-use tad\wordpress\maker\DateMaker;
-
-class UserMaker {
+class UserMaker
+{
     protected static $userRolesToLevels = ['subscriber' => '0', 'contributor' => '1', 'author' => '2', 'editor' => '7', 'administrator' => '10'];
-public static function generateUserDefaultsFrom($user_login, $user_id, $role = 'subscriber'){
-    $usersTableDefaults = array(
-        'ID' => $user_id,
-        'user_login' => $user_login,
-        'user_pass' => md5($user_login),
-        'user_nicename' => $user_login,
-        'user_email' => $user_login . "@example.com",
-        'user_url' => 'http://www.example.com',
-        'user_registered' => DateMaker::now(),
-        'user_status' => '0',
-        'display_name' => $user_login);
-    return $usersTableDefaults;
-}
-public static function generateCapabilitiesDefaultsFrom($user_id, $role){
-    $capabilitiesDefaults = array(
-        'umeta_id' => null,
-        'user_id' => $user_id,
-        'meta_key' => 'wp_capabilities',
-        'meta_value' => serialize(array($role, '1'))
-    );
-    return $capabilitiesDefaults;
-}
-public static function generateUserLevelDefaultsFrom($user_id, $role){
-    $intRole = 0;
-    if(isset(self::$userRolesToLevels[$role])){
-        $intRole = self::$userRolesToLevels[$role];
+
+    public function makeUser($user_login, $user_id, $role, $userData)
+    {
+        $userTableDefaults = self::generateUserDefaultsFrom($user_login, $user_id, $role);
+        $userCapabilitiesDefaults = self::generateCapabilitiesDefaultsFrom($user_id, $role);
+        $userLevelDefaults = self::generateUserLevelDefaultsFrom($user_id, $role);
+        // merge user data with defaults
+        $userTableData = array_merge($userTableDefaults, $userData);
+        $userCapabilitiesData = array_merge($userCapabilitiesDefaults, $userData);
+        $userLevelData = array_merge($userLevelDefaults, $userData);
+        return array($userLevelDefaults, $userTableData, $userCapabilitiesData);
     }
-    $userLevelDefaults = array(
-        'umeta_id' => null,
-        'user_id'  => $user_id,
-        'meta_key' => 'wp_user_level',
-        'meta_value' => $intRole
-    );
-    return $userLevelDefaults;
-}
+
+    protected static function generateUserDefaultsFrom($user_login, $user_id, $role = 'subscriber')
+    {
+        $usersTableDefaults = array(
+            'ID' => $user_id,
+            'user_login' => $user_login,
+            'user_pass' => md5($user_login),
+            'user_nicename' => $user_login,
+            'user_email' => $user_login . "@example.com",
+            'user_url' => 'http://www.example.com',
+            'user_registered' => DateMaker::now(),
+            'user_status' => '0',
+            'display_name' => $user_login);
+        return $usersTableDefaults;
+    }
+
+    protected static function generateCapabilitiesDefaultsFrom($user_id, $role)
+    {
+        $capabilitiesDefaults = array(
+            'umeta_id' => null,
+            'user_id' => $user_id,
+            'meta_key' => 'wp_capabilities',
+            'meta_value' => serialize(array($role, '1'))
+        );
+        return $capabilitiesDefaults;
+    }
+
+    protected static function generateUserLevelDefaultsFrom($user_id, $role)
+    {
+        $intRole = 0;
+        if (isset(self::$userRolesToLevels[$role])) {
+            $intRole = self::$userRolesToLevels[$role];
+        }
+        $userLevelDefaults = array(
+            'umeta_id' => null,
+            'user_id' => $user_id,
+            'meta_key' => 'wp_user_level',
+            'meta_value' => $intRole
+        );
+        return $userLevelDefaults;
+    }
 }
