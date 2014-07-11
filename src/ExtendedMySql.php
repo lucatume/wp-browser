86688c1b7adbe6d77831b473f3be355c5b652d7f
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-07-11 16:43:36 +0200

    added the insertOrUpdate method to the ExtendedMySql class

--- src/ExtendedMySql.php
+++ src/ExtendedMySql.php
@@ -3,6 +3,28 @@
 namespace Codeception\Lib\Driver;
 
 
-class ExtendedMySql extends  MySql{
+class ExtendedMySql extends MySql
+{
 
-} 
+    public function insertOrUpdate($tableName, array &$data)
+    {
+        $columns = array_map(
+            array($this, 'getQuotedName'),
+            array_keys($data)
+        );
+
+        $updateAssignments = array();
+        foreach ($data as $key => $value) {
+            $updateAssignments[] = sprintf('%s=%s', $key, $value);
+        }
+        $updateAssignments = implode(', ', $updateAssignments);
+
+        $query = sprintf(
+            "INSERT INTO %s (%s) VALUES (%s) ON DUPLICATE KEY UPDATE %s",
+            $this->getQuotedName($tableName),
+            implode(', ', $columns),
+            implode(', ', array_fill(0, count($data), '?')),
+            $updateAssignments
+        );
+    }
+}
