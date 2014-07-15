19caebb36750be8457b03a909bbac707b6cd26cd
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-07-15 10:50:00 +0200

    moved the haveOrUpdateInDatabase method to the Codeception\Module\ExtendedDb class

--- src/ExtendedDb.php
+++ src/ExtendedDb.php
@@ -19,4 +19,32 @@ class ExtendedDb extends Db
             $this->fail(sprintf("Record with %s couldn't be deleted from %s", json_encode($data), $table));
         }
     }
+
+    /**
+     * Inserts or updates a database entry on duplicate key.
+     *
+     * @param  string $table The table name.
+     * @param  array $data An associative array of the column names and values to insert.
+     *
+     * @return void
+     */
+    public function haveOrUpdateInDatabase($table, array $data)
+    {
+        $query = $this->driver->insertOrUpdate($table, $data);
+        $this->debugSection('Query', $query);
+
+        $sth = $this->driver->getDbh()->prepare($query);
+        if (!$sth) {
+            $this->fail("Query '$query' can't be executed.");
+        }
+        $i = 1;
+        foreach ($data as $val) {
+            $sth->bindValue($i, $val);
+            $i++;
+        }
+        $res = $sth->execute();
+        if (!$res) {
+            $this->fail(sprintf("Record with %s couldn't be inserted into %s", json_encode($data), $table));
+        }
+    }
 }
