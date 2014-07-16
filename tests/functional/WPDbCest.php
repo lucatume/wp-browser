2d971055f04c40f6622961852c88e27dc7dde46f
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-07-16 12:19:31 +0200

    added a first functional test case implementation

---
+++ tests/functional/WPDbCest.php
@@ -0,0 +1,18 @@
+<?php
+use \FunctionalTester;
+
+class WPDbCest
+{
+    public function _before()
+    {
+    }
+
+    public function _after()
+    {
+    }
+
+    // tests
+    public function tryToTest(FunctionalTester $I)
+    {
+    }
+}
