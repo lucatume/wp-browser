be2fb77b1061698b39d009a5735d3610009cb14d
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-08-21 16:33:19 +0200

    addde the WPLoader class

---
+++ src/WPLoader.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tad\wordpress\loader;
+
+/**
+ * Class WPLoader
+ *
+ * Loads a WordPress installation to make its defined functions and methods
+ * available in the global space.
+ *
+ * @package tad\wordpress
+ */
+class WPLoader {
+
+} 
