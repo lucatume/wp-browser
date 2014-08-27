b346272e15d76cc487f72726aa7384fe25ac6c28
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-08-27 18:51:50 +0200

    create the GenerateWPUnit command class

---
+++ src/GenerateWPUnit.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Codeception\Command;
+
+use Codeception\Lib\Generator\PhpUnit as PhpUnitGenerator;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Command\Command;
+
+
+/**
+ * Generates skeleton for unit test as in classical PHPUnit.
+ *
+ * * `codecept g:wpunit unit UserTest`
+ * * `codecept g:wpunit unit User`
+ * * `codecept g:wpunit unit "App\User`
+ *
+ */
+class GenerateWPUnit extends Command
+{
+    use Shared\FileSystem;
+    use Shared\Config;
+
+    const SLUG = "generate:wpunit";
+
+    protected function configure()
+    {
+        $this->setDefinition(array(
+
+            new InputArgument('suite', InputArgument::REQUIRED, 'suite where tests will be put'),
+            new InputArgument('class', InputArgument::REQUIRED, 'class name'),
+            new InputOption('config', 'c', InputOption::VALUE_OPTIONAL, 'Use custom path for config'),
+        ));
+        parent::configure();
+    }
+
+    public function getDescription() {
+        return 'Generates empty WP_UnitTestCase test without Codeception additions';
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        $suite = $input->getArgument('suite');
+        $class = $input->getArgument('class');
+
+        $config = $this->getSuiteConfig($suite, $input->getOption('config'));
+
+        $path = $this->buildPath($config['path'], $class);
+
+        $filename = $this->completeSuffix($this->getClassName($class), 'Test');
+        $filename = $path.$filename;
+
+        $gen = new PhpUnitGenerator($config, $class);
+
+        $res = $this->save($filename, $gen->produce());
+        if (!$res) {
+            $output->writeln("<error>Test $filename already exists</error>");
+            exit;
+        }
+
+        $output->writeln("<info>Test was created in $filename</info>");
+    }
+
+}
+
