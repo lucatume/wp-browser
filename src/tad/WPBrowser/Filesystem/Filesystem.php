9def1d07a03fa62ef9d71339022a5c5657446499
Zdenek <gozdenek@gmail.com>
2015-11-16 09:43:16 -0800

    For Windows deployment use Windows-specific environment properties to find user home directory

--- src/tad/WPBrowser/Filesystem/Filesystem.php
+++ src/tad/WPBrowser/Filesystem/Filesystem.php
@@ -12,7 +12,16 @@ class Filesystem extends \Symfony\Component\Filesystem\Filesystem
 
     public function getUserHome()
     {
-        $userInfo = posix_getpwuid(posix_getuid());
-        return $userInfo['dir'];
+        // on Windows the POSIX library is not implemented so use environment variables
+        if ( function_exists('posix_getpwuid') ) {
+            $userInfo = posix_getpwuid( posix_getuid() );
+            $home_dir = $userInfo['dir'];
+        } else if ( getenv("HOMEPATH") !== false ) {
+            $home_dir = getenv("HOMEDRIVE") . getenv("HOMEPATH");
+        } else {
+            throw new \RuntimeException("posix_getpwuid does not exist. Check if POSIX library is enabled.", 1);
+        }
+
+        return $home_dir;
     }
 }
