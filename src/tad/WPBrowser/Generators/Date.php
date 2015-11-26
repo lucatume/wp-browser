c79cdfd579f17798c103637e3ce3ed7c32cf84b7
Luca Tumedei <luca@theaveragedev.com>
2015-11-26 19:23:07 +0100

    updated Post and Date generators

--- src/tad/WPBrowser/Generators/Date.php
+++ src/tad/WPBrowser/Generators/Date.php
@@ -1,38 +1,52 @@
 <?php
 
-namespace tad\WPBrowser\Generators;
+	namespace tad\WPBrowser\Generators;
 
-/**
- * Generates dates in WordPress database format.
- */
-class Date
-{
-    /**
-     * The date format used in WordPress databases.
-     */
-    const DATE_FORMAT = 'Y-m-d H:i:s';
+	/**
+	 * Generates dates in WordPress database format.
+	 */
+	class Date {
+		/**
+		 * The date format used in WordPress databases.
+		 */
+		const DATE_FORMAT = 'Y-m-d H:i:s';
 
-    /**
-     * Returns the current time in WordPress specific format.
-     *
-     * @return string The current time in WordPress specific format like "2014-06-16 08:27:21"
-     */
-    public static function now()
-    {
-        return date(self::DATE_FORMAT);
-    }
+		/**
+		 * @var int An injectable time value, used in tests.
+		 */
+		protected static $time;
 
-    /**
-     * Returns the 0 time string in WordPress specific format.
-     * @return string The "0000-00-00 00:00:00" string.
-     */
-    public static function zero()
-    {
-        return '0000-00-00 00:00:00';
-    }
+		/**
+		 * Returns the current time in WordPress specific format.
+		 *
+		 * @return string The current time in WordPress specific format like "2014-06-16 08:27:21"
+		 */
+		public static function now() {
+			return date( self::DATE_FORMAT, self::_time() );
+		}
 
-    public static function gmtNow()
-    {
-        return gmdate(self::DATE_FORMAT);
-    }
-} 
+		/**
+		 * Returns the 0 time string in WordPress specific format.
+		 * @return string The "0000-00-00 00:00:00" string.
+		 */
+		public static function zero() {
+			return '0000-00-00 00:00:00';
+		}
+
+		public static function gmtNow() {
+			return gmdate( self::DATE_FORMAT, self::_time() );
+		}
+
+		public static function _injectNow( $now ) {
+			self::$time = $now;
+		}
+
+		/**
+		 * @return int
+		 */
+		public static function _time() {
+			$time = self::$time ? self::$time : time();
+
+			return $time;
+		}
+	}
--- src/tad/WPBrowser/Generators/Post.php
+++ src/tad/WPBrowser/Generators/Post.php
@@ -1,123 +1,116 @@
 <?php
 
-namespace tad\WPBrowser\Generators;
+	namespace tad\WPBrowser\Generators;
 
-use BaconStringUtils\Slugifier;
+	use BaconStringUtils\Slugifier;
 
-/**
- * Generates WordPress posts to be inserted in the database.
- */
-class Post
-{
-    protected static $count = 1;
+	/**
+	 * Generates WordPress posts to be inserted in the database.
+	 */
+	class Post {
 
-    /**
-     * Generates a WordPress page entry.
-     *
-     * @param  int $ID The post ID to use.
-     * @param  string $url The site url.
-     *
-     * @param array $data
-     * @return array Key/value pairs to be used to insert the page in the database.
-     */
-    public static function makePage($ID,
-        $url = 'http://www.example.com',
-        array $data = array())
-    {
-        $defaults = self::generateDefaultsForType($ID, 'page', $url);
-        self::$count++;
-        return array_merge($defaults, $data);
-    }
+		/**
+		 * Generates a WordPress page entry.
+		 *
+		 * @param  int $ID The post ID to use.
+		 * @param  string $url The site url.
+		 *
+		 * @param array $data
+		 *
+		 * @return array Key/value pairs to be used to insert the page in the database.
+		 */
+//		public static function makePage(
+//			$ID,
+//			$url = 'http://www.example.com',
+//			array $data = array()
+//		) {
+//			$defaults = self::getDefaults( $ID, 'page', $url );
+//			self::$count ++;
+//
+//			return array_merge( $defaults, $data );
+//		}
 
-    /**
-     * Generates a default post entry.
-     *
-     * @param  int $ID The post ID to use.
-     * @param  string $type The type (e.g. 'event') of the post that will be inserted.
-     * @param  string $url The site url
-     *
-     * @return array       Key\value pairs of post entry with default values.
-     */
-    protected static function generateDefaultsForType($ID,
-        $type,
-        $url)
-    {
-        list($title, $content) = self::generateTitleAndContent();
-        $guid = $type == 'page' ? self::generatePageGuid($ID, $url) : self::generatePostGuid($ID, $url);
-        $defaults = array(
-            'ID' => $ID,
-            'post_author' => 1,
-            'post_date' => Date::now(),
-            'post_date_gmt' => Date::gmtNow(),
-            'post_content' => $content,
-            'post_title' => $title,
-            'post_excerpt' => '',
-            'post_status' => 'publish',
-            'comment_status' => 'open',
-            'ping_status' => 'open',
-            'post_password' => '',
-            'post_name' => (new Slugifier())->slugify($title),
-            'to_ping' => '',
-            'pinged' => '',
-            'post_modified' => Date::now(),
-            'post_modified_gmt' => Date::gmtNow(),
-            'post_content_filtered' => '',
-            'post_parent' => 0,
-            'guid' => $guid,
-            'menu_order' => 0,
-            'post_type' => $type
-        );
-        return $defaults;
-    }
+		protected static function getDefaults( $id, $url ) {
+			$title    = self::generateTitle( $id );
+			$defaults = array(
+				'post_author'           => 1,
+				'post_date'             => Date::now(),
+				'post_date_gmt'         => Date::gmtNow(),
+				'post_content'          => self::generateContent( $id ),
+				'post_title'            => $title,
+				'post_excerpt'          => self::generateExcerpt( $id ),
+				'post_status'           => 'publish',
+				'comment_status'        => 'open',
+				'ping_status'           => 'open',
+				'post_password'         => '',
+				'post_name'             => ( new Slugifier() )->slugify( $title ),
+				'to_ping'               => '',
+				'pinged'                => '',
+				'post_modified'         => Date::now(),
+				'post_modified_gmt'     => Date::gmtNow(),
+				'post_content_filtered' => '',
+				'post_parent'           => 0,
+				'guid'                  => "{$url}/?p={$id}",
+				'menu_order'            => 0,
+				'post_type'             => 'post'
+			);
 
-    /**
-     * Generates random title and content for a post.
-     *
-     * @return array      Random generated title and content of the post.
-     */
-    protected static function generateTitleAndContent()
-    {
-        // create a default value array
-        $title = sprintf('Post title %d', self::$count);
-        $content = sprintf('Post content %d', self::$count);
-        return array($title, $content);
-    }
+			return $defaults;
+		}
 
-    /**
-     * Generates a page guid.
-     *
-     * @param  int $ID The page id.
-     * @param  string $url The site url.
-     *
-     * @return string      The database guid entry.
-     */
-    protected static function generatePageGuid($ID,
-        $url)
-    {
-        $guid = rtrim($url, '/') . '/?page_id=' . $ID;
-        return $guid;
-    }
+		/**
+		 * Generates a page guid.
+		 *
+		 * @param  int $ID The page id.
+		 * @param  string $url The site url.
+		 *
+		 * @return string      The database guid entry.
+		 */
+		protected static function generatePageGuid(
+			$ID,
+			$url
+		) {
+			$guid = rtrim( $url, '/' ) . '/?page_id=' . $ID;
 
-    /**
-     * Generates a post guid.
-     *
-     * @param  int $ID The post id.
-     * @param  string $url The site url.
-     *
-     * @return string      The database guid entry.
-     */
-    protected static function generatePostGuid($ID,
-        $url)
-    {
-        $guid = rtrim($url, '/') . '/?p=' . $ID;
-        return $guid;
-    }
+			return $guid;
+		}
 
-    public static function makePost($ID,
-        $url = 'http://www.example.com',
-        array $data = array())
-    {
-        $defaults = self::generateDefaultsForType($ID, 'post', $url);
-        return array_merge($defaults, $data);
-    }
-}
+		/**
+		 * Generates a post guid.
+		 *
+		 * @param  int $ID The post id.
+		 * @param  string $url The site url.
+		 *
+		 * @return string      The database guid entry.
+		 */
+		protected static function generatePostGuid(
+			$ID,
+			$url
+		) {
+			$guid = rtrim( $url, '/' ) . '/?p=' . $ID;
+
+			return $guid;
+		}
+
+		public static function makePost( $id, $url = 'http://www.example.com', array $data = [ ] ) {
+			return array_merge( self::getDefaults( $id, $url ), $data );
+		}
+
+		/**
+		 * @return string
+		 */
+		protected static function generateTitle( $id ) {
+			return sprintf( 'Post %d title', $id );
+		}
+
+		/**
+		 * @return string
+		 */
+		protected static function generateContent( $id ) {
+			return sprintf( 'Post %d content', $id );
+		}
+
+		private static function generateExcerpt( $id ) {
+			return sprintf( 'Post %d excerpt', $id );
+		}
+	}
