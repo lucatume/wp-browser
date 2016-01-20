245e8aa75199997ef8ec815bd318c432347fefd6
Luca Tumedei <luca@theaveragedev.com>
2016-01-20 19:16:33 +0100

    added wpunit suite to bootstrap

--- src/Codeception/Command/WPBootstrap.php
+++ src/Codeception/Command/WPBootstrap.php
@@ -2,6 +2,9 @@
 
 namespace Codeception\Command;
 
+use Symfony\Component\Console\Input\ArrayInput;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Yaml\Yaml;
 
 class WPBootstrap extends Bootstrap
@@ -102,6 +105,86 @@ class WPBootstrap extends Bootstrap
         $this->createSuite('functional', $actor, $str);
     }
 
+    public function execute( InputInterface $input, OutputInterface $output ) {
+        if ($input->getOption('namespace')) {
+            $this->namespace = trim($input->getOption('namespace'), '\\') . '\\';
+        }
+
+        if ($input->getOption('actor')) {
+            $this->actorSuffix = $input->getOption('actor');
+        }
+
+        $path = $input->getArgument('path');
+
+        if (!is_dir($path)) {
+            $output->writeln("<error>\nDirectory '$path' does not exist\n</error>");
+            return;
+        }
+
+        $realpath = realpath($path);
+        chdir($path);
+
+        if (file_exists('codeception.yml')) {
+            $output->writeln("<error>\nProject is already initialized in '$path'\n</error>");
+            return;
+        }
+
+        $output->writeln(
+            "<fg=white;bg=magenta> Initializing Codeception in " . $realpath . " </fg=white;bg=magenta>\n"
+        );
+
+        $this->createGlobalConfig();
+        $output->writeln("File codeception.yml created       <- global configuration");
+
+        $this->createDirs();
+
+        if (!$input->getOption('empty')) {
+            $this->createUnitSuite();
+            $output->writeln("tests/unit created                 <- unit tests");
+            $output->writeln("tests/unit.suite.yml written       <- unit tests suite configuration");
+            $this->createWpunitSuite();
+            $output->writeln("tests/wpunit created                 <- WordPress unit tests");
+            $output->writeln("tests/wpunit.suite.yml written       <- WordPress unit tests suite configuration");
+            $this->createFunctionalSuite();
+            $output->writeln("tests/functional created           <- functional tests");
+            $output->writeln("tests/functional.suite.yml written <- functional tests suite configuration");
+            $this->createAcceptanceSuite();
+            $output->writeln("tests/acceptance created           <- acceptance tests");
+            $output->writeln("tests/acceptance.suite.yml written <- acceptance tests suite configuration");
+        }
+
+        if (file_exists('.gitignore')) {
+            file_put_contents('tests/_output/.gitignore', '');
+            file_put_contents('.gitignore', file_get_contents('.gitignore') . "\ntests/_output/*");
+            $output->writeln("tests/_output was added to .gitignore");
+        }
+
+        $output->writeln(" --- ");
+        $this->ignoreFolderContent('tests/_output');
+
+        file_put_contents('tests/_bootstrap.php', "<?php\n// This is global bootstrap for autoloading\n");
+        $output->writeln("tests/_bootstrap.php written <- global bootstrap file");
+
+        $output->writeln("<info>Building initial {$this->actorSuffix} classes</info>");
+        $this->getApplication()->find('build')->run(
+            new ArrayInput(['command' => 'build']),
+            $output
+        );
+
+        $output->writeln("<info>\nBootstrap is done. Check out " . $realpath . "/tests directory</info>");
+    }
+
+    protected function createWpunitSuite($actor = 'Wpunit')
+    {
+        $suiteConfig = $this->getWpunitSuiteConfig($actor);
+
+        $str = "# Codeception Test Suite Configuration\n\n";
+        $str .= "# suite for WordPress unit tests.\n";
+        $str .= "# Load WordPress and unit test classes that rely on it.\n";
+        $str .= Yaml::dump($suiteConfig, 2);
+        $this->createSuite('wpunit', $actor, $str);
+    }
+
     protected function createAcceptanceSuite($actor = 'Acceptance')
     {
         $suiteConfig = $this->getAcceptanceSuiteConfig($actor);
@@ -126,6 +209,26 @@ class WPBootstrap extends Bootstrap
             'modules' => [
                 'enabled' => [
                     'Filesystem',
+                    'WPLoader',
+                    "\\{$this->namespace}Helper\\{$actor}"
+                ]
+            ]
+        ];
+
+        return $suiteConfig;
+    }
+
+    /**
+     * @param $actor
+     *
+     * @return array
+     */
+    protected function getWpunitSuiteConfig($actor)
+    {
+        $suiteConfig = [
+            'class_name' => $actor . $this->actorSuffix,
+            'modules' => [
+                'enabled' => [
                     'WPLoader',
                     "\\{$this->namespace}Helper\\{$actor}"
                 ]
