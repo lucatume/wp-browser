9a5ece72e146ee5ee3e4c7636f33553c99ebe8f8
Luca Tumedei <luca@theaveragedev.com>
2016-05-03 18:39:12 +0200

    added some tests for the core suite integration

---
+++ tests/wploader/factoriesTest.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class factoriesTest extends \Codeception\TestCase\WPTestCase {
+
+	public function setUp() {
+		// before
+		parent::setUp();
+
+		// your set up methods here
+	}
+
+	public function tearDown() {
+		// your tear down methods here
+
+		// then
+		parent::tearDown();
+	}
+
+	/**
+	 * @test
+	 * it should allow using the posts factory
+	 */
+	public function it_should_allow_using_the_posts_factory() {
+		$this->factory()->post->create_many( 10, [ 'post_type' => 'post' ] );
+
+		$this->assertCount( 10, get_posts( [ 'nopaging' => true, 'post_type' => 'post' ] ) );
+	}
+}
---
+++ tests/wploader/wpdbAccessTest.php
@@ -0,0 +1,42 @@
+<?php
+
+
+class wpdbAccessTest extends \Codeception\TestCase\WPTestCase {
+
+	public function setUp() {
+		// before
+		parent::setUp();
+
+		// your set up methods here
+	}
+
+	public function tearDown() {
+		// your tear down methods here
+
+		// then
+		parent::tearDown();
+	}
+
+	/**
+	 * @test
+	 * it should allow accessing the wpdb instance in tests
+	 */
+	public function it_should_allow_accessing_the_wpdb_instance_in_tests() {
+		/** @var \wpdb $wpdb */
+		global $wpdb;
+
+		$this->assertNotEmpty( $wpdb );
+		$this->assertInstanceOf( 'wpdb', $wpdb );
+	}
+
+	/**
+	 * @test
+	 * it should allow running queries using wpdb
+	 */
+	public function it_should_allow_running_queries_using_wpdb() {
+		/** @var \wpdb $wpdb */
+		global $wpdb;
+
+		$wpdb->get_results( "select * from {$wpdb->posts}" );
+	}
+}
