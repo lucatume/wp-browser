623b8f2ee1c945f65bce6d486297a4483c240b3b
Luca Tumedei <luca@theaveragedev.com>
2016-05-25 07:02:28 +0200

    updated \wpdb class shim

--- tests/_data/classes/wpdb.php
+++ tests/_data/classes/wpdb.php
@@ -1,8 +1,12 @@
 <?php
 
 if (!class_exists('wpdb')) {
+    /**
+     * A \wpdb class shim for testing purposes.
+     */
     class wpdb
     {
+        private $result = null;
         public $queries = [];
     }
 }
