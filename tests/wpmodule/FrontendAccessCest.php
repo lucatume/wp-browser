704d9536c1d251369070cc0394324dd9ea72be36
Luca Tumedei <luca@theaveragedev.com>
2016-06-22 18:05:39 +0200

    udpated tests

--- tests/wpmodule/FrontendAccessCest.php
+++ tests/wpmodule/FrontendAccessCest.php
@@ -13,5 +13,43 @@ class FrontendAccessCest
     {
         $I->amOnPage('/');
     }
+
+    /**
+     * @test
+     * it should be able to insert a post in the database
+     */
+    public function it_should_be_able_to_insert_a_post_in_the_database(WpmoduleTester $I)
+    {
+        $I->factory()->post->create(['post_title' => 'A post']);
+        $I->amOnPage('/');
+        $I->see('A post');
+    }
+
+    /**
+     * @test
+     * it should be able to navigate to a single post page
+     */
+    public function it_should_be_able_to_navigate_to_a_single_post_page(WpmoduleTester $I)
+    {
+        $id = $I->factory()->post->create(['post_title' => 'A post']);
+        $I->amOnPage('/?p=' . $id);
+        $I->see('A post');
+        $I->seeElement('body.single');
+    }
+
+    /**
+     * @test
+     * it should allow setting permalinks structure in tests
+     */
+    public function it_should_allow_setting_permalink_structure_in_tests(WpmoduleTester $I)
+    {
+        $I->factory()->post->create(['post_title' => 'A post', 'post_name' => 'a-post']);
+
+        $I->setPermalinkStructure('/%postname%/');
+
+        $I->amOnPage('/a-post');
+        $I->seeElement('body.single');
+    }
+
 }
 
