0fe54254d2b2a308288489ee4f1ce98763676cd3
Luca Tumedei <luca@theaveragedev.com>
2016-06-23 17:51:58 +0200

    updated WordPress module tests

--- tests/wpmodule/FrontendAccessCest.php
+++ tests/wpmodule/FrontendAccessCest.php
@@ -16,21 +16,9 @@ class FrontendAccessCest
 
     /**
      * @test
-     * it should be able to insert a post in the database
+     * it should be able to insert a post in the database and see it on the homepage
      */
-    public function it_should_be_able_to_insert_a_post_in_the_database(WpmoduleTester $I)
-    {
-        wp_insert_post(['post_type' => 'post', 'post_title' => 'A post', 'post_status' => 'publish']);
-        $I->amOnPage('/');
-        $I->see('A post');
-    }
-
-
-    /**
-     * @test
-     * it should be able to use WPDb like methods to manipulate the database
-     */
-    public function it_should_be_able_to_use_wp_db_like_methods_to_manipulate_the_database(WpmoduleTester $I)
+    public function it_should_be_able_to_insert_a_post_in_the_database_and_see_it_on_the_homepage(WpmoduleTester $I)
     {
         $I->havePostInDatabase(['post_type' => 'post', 'post_title' => 'A post', 'post_status' => 'publish']);
         $I->amOnPage('/');
