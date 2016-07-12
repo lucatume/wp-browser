d18ec0793d4bcc56aa5bb0fc4c698d7793c02e13
Luca Tumedei <luca@theaveragedev.com>
2016-07-12 19:14:14 +0200

    added doc line

--- src/tad/scripts/filters.php
+++ src/tad/scripts/filters.php
@@ -1,4 +1,8 @@
 <?php
+
+/**
+ * Filter the `status_header` actiion to log response stati set during WordPress execution.
+ */
 tests_add_filter('status_header', function ($_, $code) {
     global $cli_statuses;
     $cli_statuses[] = $code;
