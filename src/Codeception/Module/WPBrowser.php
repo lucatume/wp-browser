188de34a5046fdcc1ad8fb2803146a029544103b
Ippei Sumida <ippey.s@me.com>
2016-08-31 04:39:16 +0900

    added following override methods in WPBrowser - activatePlugin - deactivatePlugin

--- src/Codeception/Module/WPBrowser.php
+++ src/Codeception/Module/WPBrowser.php
@@ -95,4 +95,20 @@ class WPBrowser extends PhpBrowser
 
         parent::validateConfig();
     }
+
+    public function activatePlugin($pluginSlug)
+    {
+        $this->checkOption('//*[@data-slug="' . $pluginSlug . '"]/th/input');
+        $this->selectOption('action', 'activate-selected');
+        $this->click("doaction");
+    }
+
+    public function deactivatePlugin($pluginSlug)
+    {
+        $this->checkOption('//*[@data-slug="' . $pluginSlug . '"]/th/input');
+        $this->selectOption('action', 'deactivate-selected');
+        $this->click("doaction");
+    }
+
+
 }
