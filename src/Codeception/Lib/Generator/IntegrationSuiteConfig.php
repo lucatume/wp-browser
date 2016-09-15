85063075d17e84e4433b7e3f932a0de6e2c0ab2e
Luca Tumedei <luca@theaveragedev.com>
2016-09-15 02:16:34 +0200

    added ignore mapping in test folder

---
+++ src/Codeception/Lib/Generator/IntegrationSuiteConfig.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Codeception\Lib\Generator;
+
+
+class IntegrationSuiteConfig extends AbstractGenerator implements GeneratorInterface
+{
+
+    public static $requiredSettings = ['className', 'namespace', 'actor'];
+
+    protected $template = <<< YAML
+class_name: {{className}}
+modules:
+    enabled:
+        - \\{{namespace}}Helper\\{{actor}}
+        - WPLoader
+    config:
+        WPLoader:
+            wpRootFolder: {{wpRootFolder}}
+            dbName: {{dbName}}
+            dbHost: {{dbHost}}
+            dbUser: {{dbUser}}
+            dbPassword: {{dbPassword}}
+            tablePrefix: {{tablePrefix}}
+            domain: {{domain}}
+            adminEmail: {{adminEmail}}
+            title: WP Tests
+            plugins: {{plugins}}
+            activatePlugins: {{plugins}}
+            bootstrapActions: []
+YAML;
+}
