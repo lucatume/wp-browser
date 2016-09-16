c619f826d54b1873d458c847ebcb36d93cf3558a
Luca Tumedei <luca@theaveragedev.com>
2016-09-16 21:42:29 +0200

    fixed syntax issue

--- tests/_support/functions.php
+++ tests/_support/functions.php
@@ -2,31 +2,31 @@
 
 namespace tad\WPBrowser\Tests\Support;
 
-function rrmdir($src)
-{
-    if (!file_exists($src)) {
-        return;
-    }
+function rrmdir($src) {
+	if (!file_exists($src)) {
+		return;
+	}
 
-    $dir = opendir($src);
-    while (false !== ($file = readdir($dir))) {
-        if (($file != '.') && ($file != '..')) {
-            $full = $src . '/' . $file;
-            if (is_dir($full)) {
-                rrmdir($full);
-            } else {
-                unlink($full);
-            }
-        }
-    }
-    closedir($dir);
-    rmdir($src);
+	$dir = opendir($src);
+	while (false !== ($file = readdir($dir))) {
+		if (($file != '.') && ($file != '..')) {
+			$full = $src . '/' . $file;
+			if (is_dir($full)) {
+				rrmdir($full);
+			} else {
+				unlink($full);
+			}
+		}
+	}
+	closedir($dir);
+	rmdir($src);
 }
 
-function importDump($dumpFile, $dbName, $dbUser = 'root', $dbPass = 'root', $dbHost = 'localhost')
-{   $commandTemplate = 'mysql -h%s -u%s %s %s < %s';
-    $command = sprintf($commandTemplate, $dbHost, $dbUser , $dbPass ? '-p' . $dbPass : '',  $dbName, $dumpFile;
-    exec($command, $output, $status);
+function importDump($dumpFile, $dbName, $dbUser = 'root', $dbPass = 'root', $dbHost = 'localhost') {
+	$commandTemplate = 'mysql -h%s -u%s %s %s < %s';
+	$dbPassEntry = $dbPass ? '-p' . $dbPass : '';
+	$command = sprintf($commandTemplate, $dbHost, $dbUser, $dbPassEntry, $dbName, $dumpFile);
+	exec($command, $output, $status);
 
-    return $status;
+	return $status;
 }
