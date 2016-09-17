53e9617a5acee92473018209c46bbc225c677ad8
Luca Tumedei <luca@theaveragedev.com>
2016-09-17 21:28:11 +0200

    pdo based import

--- tests/_support/functions.php
+++ tests/_support/functions.php
@@ -2,31 +2,43 @@
 
 namespace tad\WPBrowser\Tests\Support;
 
-function rrmdir($src) {
-	if (!file_exists($src)) {
-		return;
-	}
+function rrmdir($src)
+{
+    if (!file_exists($src)) {
+        return;
+    }
 
-	$dir = opendir($src);
-	while (false !== ($file = readdir($dir))) {
-		if (($file != '.') && ($file != '..')) {
-			$full = $src . '/' . $file;
-			if (is_dir($full)) {
-				rrmdir($full);
-			} else {
-				unlink($full);
-			}
-		}
-	}
-	closedir($dir);
-	rmdir($src);
+    $dir = opendir($src);
+    while (false !== ($file = readdir($dir))) {
+        if (($file != '.') && ($file != '..')) {
+            $full = $src . '/' . $file;
+            if (is_dir($full)) {
+                rrmdir($full);
+            } else {
+                unlink($full);
+            }
+        }
+    }
+    closedir($dir);
+    rmdir($src);
 }
 
-function importDump($dumpFile, $dbName, $dbUser = 'root', $dbPass = 'root', $dbHost = 'localhost', &$output = null) {
-	$commandTemplate = 'mysql --host=%s --user=%s %s %s < %s';
-	$dbPassEntry = $dbPass ? '--password=' . $dbPass : '';
-	$command = sprintf($commandTemplate, $dbHost, $dbUser, $dbPassEntry, $dbName, $dumpFile);
-	exec($command, $output, $status);
+function importDump($dumpFile, $dbName, $dbUser = 'root', $dbPass = 'root', $dbHost = 'localhost', &$output = null)
+{
+    $dumpFileContents = file_get_contents($dumpFile);
+    if (empty($dumpFileContents)) {
+        return -1;
+    }
 
-	return $status;
+    $dsn = sprintf('mysql:host=%s;dbname=%s', $dbHost, $dbName);
+    $db = new \PDO($dsn, $dbUser, $dbPass);
+
+    $mysqlVersion = $db->getAttribute(\PDO::ATTR_CLIENT_VERSION);
+
+    if (version_compare($mysqlVersion, '5.5.3', '<')) {
+        $dumpFileContents = preg_replace('/CHARSET=(utf8[^\\s]*)/', 'CHARSET=utf8', $dumpFileContents);
+        $dumpFileContents = preg_replace('/COLLATE=(utf8[^\\s]*)/', 'COLLATE=utf8_bin', $dumpFileContents);
+    }
+
+    return false !== $db->exec($dumpFileContents);
 }
