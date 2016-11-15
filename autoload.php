2faa972e17c26c598190052175ea2b517f3149a6
Luca Tumedei <luca@theaveragedev.com>
2016-11-15 16:34:00 +0100

    code format

--- autoload.php
+++ autoload.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__FILE__). 'vendor/autoload.php';
+require_once dirname(__FILE__) . 'vendor/autoload.php';
 
 // Include Codeception own autoload.php file
 require_once(wpbrowser_vendor_path('codeception/codeception/autoload.php'));
