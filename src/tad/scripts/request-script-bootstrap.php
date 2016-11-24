ac59ab732faaf827297572f37e18a15881f1a17e
Luca Tumedei <luca@theaveragedev.com>
2016-11-24 15:21:42 +0100

    add empty checks on packets coming in

--- src/tad/scripts/request-script-bootstrap.php
+++ src/tad/scripts/request-script-bootstrap.php
@@ -10,31 +10,50 @@ $indexFile = $argv[1];
 
 $env = unserialize(base64_decode($argv[2]));
 
-foreach ($env['cookie'] as $key => $value) {
-	$_COOKIE[$key] = $value;
+if (!empty($env['cookie'])) {
+    foreach ($env['cookie'] as $key => $value) {
+        $_COOKIE[$key] = $value;
+    }
 }
 
-foreach ($env['server'] as $key => $value) {
-	$_SERVER[$key] = $value;
+if (!empty($env['server'])) {
+    foreach ($env['server'] as $key => $value) {
+        $_SERVER[$key] = $value;
+    }
 }
-foreach ($env['files'] as $key => $value) {
-	$_FILES[$key] = $value;
+
+if (!empty($env['files'])) {
+    foreach ($env['files'] as $key => $value) {
+        $_FILES[$key] = $value;
+    }
 }
-foreach ($env['request'] as $key => $value) {
-	$_REQUEST[$key] = $value;
+
+if (!empty($env['request'])) {
+    foreach ($env['request'] as $key => $value) {
+        $_REQUEST[$key] = $value;
+    }
 }
+
 if (!empty($env['get'])) {
-	foreach ($env['get'] as $key => $value) {
-		$_GET[$key] = $value;
-	}
+    if (!empty($env['get'])) {
+        foreach ($env['get'] as $key => $value) {
+            $_GET[$key] = $value;
+        }
+    }
 }
-if (!empty($env['post'])) {
-	foreach ($env['post'] as $key => $value) {
-		$_POST[$key] = $value;
-	}
+
+if (!empty($env['post')) {
+    if (!empty($env['post'])) {
+        foreach ($env['post'] as $key => $value) {
+            $_POST[$key] = $value;
+        }
+    }
 }
-foreach ($env['headers'] as $header) {
-	header($header);
+
+if (!empty($env['headers'])) {
+    foreach ($env['headers'] as $header) {
+        header($header);
+    }
 }
 
 // disable CRON tasks to avoid parallel processes running on an empty database
