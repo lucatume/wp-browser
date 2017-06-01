bcad5359f1bf0c75a7c7b01e2cbde98f4cf25425
Luca Tumedei <luca@theaveragedev.com>
2017-06-01 12:05:25 +0200

    fix missing command interface

--- src/Codeception/Command/DbSnapshot.php
+++ src/Codeception/Command/DbSnapshot.php
@@ -2,6 +2,7 @@
 
 namespace Codeception\Command;
 
+use Codeception\CustomCommandInterface;
 use Symfony\Component\Console\Exception\RuntimeException;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -12,147 +13,156 @@ use tad\WPBrowser\Filesystem\Filesystem;
 use tad\WPBrowser\Services\Db\MySQLDumpFactory;
 use tad\WPBrowser\Services\Db\MySQLDumpFactoryInterface;
 
-class DbSnapshot extends BaseCommand
-{
-	/**
-	 * @var MySQLDumpFactoryInterface
-	 */
-	protected $pdoFactory;
+class DbSnapshot extends BaseCommand implements CustomCommandInterface {
 
-	/**
-	 * @var \MySQLDump
-	 */
-	protected $dump;
-	/**
-	 * @var Filesystem
-	 */
-	private $filesystem;
+    /**
+     * @var MySQLDumpFactoryInterface
+     */
+    protected $pdoFactory;
 
-	public function __construct(
-		$name = null,
-		MySQLDumpFactoryInterface $pdoFactory = null,
-		Filesystem $filesystem = null
-	) {
-		parent::__construct($name);
-		$this->pdoFactory = $pdoFactory ? $pdoFactory : new MySQLDumpFactory();
-		$this->filesystem = $filesystem ? $filesystem : new Filesystem();
-	}
+    /**
+     * @var \MySQLDump
+     */
+    protected $dump;
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
 
-	/**
-	 * Returns the `MySQLDump::tables` attribute.
-	 *
-	 * Used for internal check operations.
-	 *
-	 * @return array
-	 */
-	public function _getDumpTables()
-	{
-		return empty($this->dump) ? [] : $this->dump->tables;
-	}
+    public function __construct(
+        $name = null,
+        MySQLDumpFactoryInterface $pdoFactory = null,
+        Filesystem $filesystem = null
+    ) {
+        parent::__construct($name);
+        $this->pdoFactory = $pdoFactory ? $pdoFactory : new MySQLDumpFactory();
+        $this->filesystem = $filesystem ? $filesystem : new Filesystem();
+    }
 
-	protected function configure()
-	{
-		$this->setName('db:snapshot')
-			->setDescription('Takes a snapshot of a database to be shared as a fixture.')
-			->addArgument('snapshot', InputArgument::REQUIRED,
-				'Specifies the filename (without extension) of the snapshot files.')
-			->addArgument('name', InputArgument::REQUIRED, 'Specifies the name of the database to snapshot.')
-			->addOption('host', null, InputOption::VALUE_OPTIONAL,
-				'If set the specified host will be used to connect to the database', 'localhost')
-			->addOption('user', 'u', InputOption::VALUE_OPTIONAL,
-				'If set the specified user will be used to connect to the database', 'root')
-			->addOption('pass', 'p', InputOption::VALUE_OPTIONAL,
-				'If set the specified password will be used to connect to the database', 'root')
-			->addOption('dump-file', null, InputOption::VALUE_OPTIONAL,
-				'If set the local version of the database will be dumped in the specified file; should be absolute path or a path relative to the root folder.')
-			->addOption('dist-dump-file', null, InputOption::VALUE_OPTIONAL,
-				'If set the distribution version of the database will be dumped in the specified file; should be absolute path or a path relative to the root folder.')
-			->addOption('skip-tables', null, InputOption::VALUE_OPTIONAL,
-				'A comma separated list of tables that should not be included in the dump.')
-			->addOption('local-url', null, InputOption::VALUE_OPTIONAL,
-				'The local setup domain that should be replaced in the distribution version of the dump file.',
-				'http://local.dev')
-			->addOption('dist-url', null, InputOption::VALUE_OPTIONAL,
-				'The distribution setup domain that should be used in the distribution version of the dump file.',
-				'http://dist.dev');
+    /**
+     * returns the name of the command
+     *
+     * @return string
+     */
+    public static function getCommandName() {
+        return 'db:snapshot';
+    }
 
-		parent::configure();
-	}
+    /**
+     * Returns the `MySQLDump::tables` attribute.
+     *
+     * Used for internal check operations.
+     *
+     * @return array
+     */
+    public function _getDumpTables() {
+        return empty($this->dump) ? [] : $this->dump->tables;
+    }
 
-	protected function execute(InputInterface $input, OutputInterface $output)
-	{
-		$host = $input->getOption('host');
-		$user = $input->getOption('user');
-		$pass = $input->getOption('pass');
-		$dbName = $input->getArgument('name');
+    protected function configure() {
+        $this->setName('db:snapshot')
+             ->setDescription('Takes a snapshot of a database to be shared as a fixture.')
+             ->addArgument('snapshot', InputArgument::REQUIRED,
+                 'Specifies the filename (without extension) of the snapshot files.')
+             ->addArgument('name', InputArgument::REQUIRED, 'Specifies the name of the database to snapshot.')
+             ->addOption('host', null, InputOption::VALUE_OPTIONAL,
+                 'If set the specified host will be used to connect to the database', 'localhost')
+             ->addOption('user', 'u', InputOption::VALUE_OPTIONAL,
+                 'If set the specified user will be used to connect to the database', 'root')
+             ->addOption('pass', 'p', InputOption::VALUE_OPTIONAL,
+                 'If set the specified password will be used to connect to the database', 'root')
+             ->addOption('dump-file', null, InputOption::VALUE_OPTIONAL,
+                 'If set the local version of the database will be dumped in the specified file; should be absolute path or a path relative to the root folder.')
+             ->addOption('dist-dump-file', null, InputOption::VALUE_OPTIONAL,
+                 'If set the distribution version of the database will be dumped in the specified file; should be absolute path or a path relative to the root folder.')
+             ->addOption('skip-tables', null, InputOption::VALUE_OPTIONAL,
+                 'A comma separated list of tables that should not be included in the dump.')
+             ->addOption('local-url', null, InputOption::VALUE_OPTIONAL,
+                 'The local setup domain that should be replaced in the distribution version of the dump file.',
+                 'http://local.dev')
+             ->addOption('dist-url', null, InputOption::VALUE_OPTIONAL,
+                 'The distribution setup domain that should be used in the distribution version of the dump file.',
+                 'http://dist.dev');
 
-		try {
-			$this->dump = $this->pdoFactory->makeDump($host, $user, $pass, $dbName);
-		} catch (\PDOException $e) {
-			throw new RuntimeException('Error while connecting to database [' . $dbName . ']: ' . $e->getMessage());
-		}
+        parent::configure();
+    }
 
-		if (false === $this->dump) {
-			$output->writeln('<error>Something went wrong with the dump component instance.</error>');
-			return false;
-		}
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $host = $input->getOption('host');
+        $user = $input->getOption('user');
+        $pass = $input->getOption('pass');
+        $dbName = $input->getArgument('name');
 
-		\Codeception\Configuration::config();
+        try {
+            $this->dump = $this->pdoFactory->makeDump($host, $user, $pass, $dbName);
+        } catch (\PDOException $e) {
+            throw new RuntimeException('Error while connecting to database [' . $dbName . ']: ' . $e->getMessage());
+        }
 
-		if (!empty($input->getOption('dump-file'))) {
-			$dumpFile = $input->getOption('dump-file');
-		} else {
-			$dumpFile = codecept_data_dir($input->getArgument('snapshot') . '.sql');
-		}
+        if (false === $this->dump) {
+            $output->writeln('<error>Something went wrong with the dump component instance.</error>');
 
-		$output->writeln('<info>Dump file will be written to [' . $dumpFile . ']</info>');
+            return false;
+        }
 
-		if (!empty($input->getOption('dist-dump-file'))) {
-			$distDumpFile = $input->getOption('dist-dump-file');
-		} else {
-			$distDumpFile = codecept_data_dir($input->getArgument('snapshot') . '.dist.sql');
-		}
+        \Codeception\Configuration::config();
 
-		$output->writeln('<info>Distribution version of dump file will be written to [' . $distDumpFile . ']</info>');
+        if ( ! empty($input->getOption('dump-file'))) {
+            $dumpFile = $input->getOption('dump-file');
+        } else {
+            $dumpFile = codecept_data_dir($input->getArgument('snapshot') . '.sql');
+        }
 
-		$skipTables = $input->getOption('skip-tables');
-		if (!empty($skipTables)) {
-			$tables = explode(',', $skipTables);
-			foreach ($tables as $table) {
-				$this->dump->tables[$table] = \MySQLDump::NONE;
-			}
-		}
+        $output->writeln('<info>Dump file will be written to [' . $dumpFile . ']</info>');
 
-		$memory = fopen('php://memory', 'w');
+        if ( ! empty($input->getOption('dist-dump-file'))) {
+            $distDumpFile = $input->getOption('dist-dump-file');
+        } else {
+            $distDumpFile = codecept_data_dir($input->getArgument('snapshot') . '.dist.sql');
+        }
 
-		$this->dump->write($memory);
+        $output->writeln('<info>Distribution version of dump file will be written to [' . $distDumpFile . ']</info>');
 
-		rewind($memory);
-		$dumpContents = stream_get_contents($memory);
+        $skipTables = $input->getOption('skip-tables');
+        if ( ! empty($skipTables)) {
+            $tables = explode(',', $skipTables);
+            foreach ($tables as $table) {
+                $this->dump->tables[$table] = \MySQLDump::NONE;
+            }
+        }
 
-		if (!$this->filesystem->file_put_contents($dumpFile, $dumpContents)) {
-			$output->writeln('<error>Could not write dump to [' . $dumpFile . ']</error>');
-			return false;
-		}
+        $memory = fopen('php://memory', 'w');
 
-		$output->writeln('<info>Dump file written to [' . $dumpFile . ']</info>');
+        $this->dump->write($memory);
 
-		$localUrl = $input->getOption('local-url');
-		$distUrl = $input->getOption('dist-url');
-		$localDomain = rtrim(preg_replace('~http(s)*:\\/\\/(www\\.)*~', '', $localUrl), '/');
-		$distDomain = rtrim(preg_replace('~http(s)*:\\/\\/(www\\.)*~', '', $distUrl), '/');
-		$distDumpContents = str_replace($localDomain, $distDomain, $dumpContents);
+        rewind($memory);
+        $dumpContents = stream_get_contents($memory);
 
-		if (!$this->filesystem->file_put_contents($distDumpFile, $distDumpContents)) {
-			$output->writeln('<error>Could not write dist dump to [' . $distDumpFile . ']</error>');
-			return false;
-		}
+        if ( ! $this->filesystem->file_put_contents($dumpFile, $dumpContents)) {
+            $output->writeln('<error>Could not write dump to [' . $dumpFile . ']</error>');
 
-		$output->writeln('<info>Distribution version of dump file written to [' . $distDumpFile . ']</info>');
-		$output->writeln('<comment>Any occurrence of [' . $localDomain . '] in it was replaced with [' . $distDomain . ']</comment>');
+            return false;
+        }
 
-		parent::execute($input, $output);
+        $output->writeln('<info>Dump file written to [' . $dumpFile . ']</info>');
 
-		return true;
-	}
+        $localUrl = $input->getOption('local-url');
+        $distUrl = $input->getOption('dist-url');
+        $localDomain = rtrim(preg_replace('~http(s)*:\\/\\/(www\\.)*~', '', $localUrl), '/');
+        $distDomain = rtrim(preg_replace('~http(s)*:\\/\\/(www\\.)*~', '', $distUrl), '/');
+        $distDumpContents = str_replace($localDomain, $distDomain, $dumpContents);
+
+        if ( ! $this->filesystem->file_put_contents($distDumpFile, $distDumpContents)) {
+            $output->writeln('<error>Could not write dist dump to [' . $distDumpFile . ']</error>');
+
+            return false;
+        }
+
+        $output->writeln('<info>Distribution version of dump file written to [' . $distDumpFile . ']</info>');
+        $output->writeln('<comment>Any occurrence of [' . $localDomain . '] in it was replaced with [' . $distDomain . ']</comment>');
+
+        parent::execute($input, $output);
+
+        return true;
+    }
 }
