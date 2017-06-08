361f1046c1bebe3c2b1652e6344b19212ac0da7f
Luca Tumedei <luca@theaveragedev.com>
2017-06-08 18:17:52 +0200

    more tests on optional paths

--- src/Codeception/Module/WPFilesystem.php
+++ src/Codeception/Module/WPFilesystem.php
@@ -5,6 +5,7 @@ namespace Codeception\Module;
 
 
 use Codeception\Exception\ModuleConfigException;
+use tad\WPBrowser\Filesystem\Utils;
 
 class WPFilesystem extends Filesystem {
 
@@ -12,18 +13,55 @@ class WPFilesystem extends Filesystem {
 
     public function _initialize() {
         $this->ensureWpRootFolder();
+        $this->ensureAndSetOptionalPaths();
     }
 
     protected function ensureWpRootFolder() {
-        $message =
-            "[{$this->config['wpRootFolder']}] is not a valid WordPress root folder.\n\nThe WordPress root folder is the one that contains the 'wp-load.php' file.";
+        $wpRoot = $this->config['wpRootFolder'];
 
-        if ( ! (is_dir($this->config['wpRootFolder']) && is_readable($this->config['wpRootFolder']) && is_writable($this->config['wpRootFolder']))) {
+        if ( ! is_dir($wpRoot)) {
+            $wpRoot = codecept_root_dir(Utils::unleadslashit($wpRoot));
+        }
+
+        $message = "[{$wpRoot}] is not a valid WordPress root folder.\n\nThe WordPress root folder is the one that contains the 'wp-load.php' file.";
+
+        if ( ! (is_dir($wpRoot) && is_readable($wpRoot) && is_writable($wpRoot))) {
             throw new ModuleConfigException(__CLASS__, $message);
         }
 
-        if ( ! file_exists($this->config['wpRootFolder'] . '/wp-load.php')) {
+        if ( ! file_exists($wpRoot . '/wp-load.php')) {
             throw new ModuleConfigException(__CLASS__, $message);
+        }
+
+        $this->config['wpRootFolder'] = Utils::untrailslashit($wpRoot) . DIRECTORY_SEPARATOR;
+    }
+
+    protected function ensureAndSetOptionalPaths() {
+        $optionalPaths = [
+            'themes'     => ['mustExist' => true, 'default' => '/wp-content/themes'],
+            'plugins'    => ['mustExist' => true, 'default' => '/wp-content/plugins'],
+            'mu-plugins' => ['mustExist' => false, 'default' => '/wp-content/mu-plugins'],
+            'uploads'    => ['mustExist' => true, 'default' => '/wp-content/uploads'],
+        ];
+        $wpRoot = $this->config['wpRootFolder'];
+        foreach ($optionalPaths as $configKey => $info) {
+            if (empty($this->config[$configKey])) {
+                $path = $info['default'];
+            } else {
+                $path = $this->config[$configKey];
+            }
+            if ( ! is_dir($path)) {
+                $path = Utils::unleadslashit($path);
+                $absolutePath = $wpRoot . $path;
+            } else {
+                $absolutePath = $path;
+            }
+            $mustExistAndIsNotDir = $info['mustExist'] && ! is_dir($absolutePath);
+            $canNotExistButIsDefined = ! empty($this->config[$configKey]) && ! is_dir($absolutePath);
+            if ($mustExistAndIsNotDir || $canNotExistButIsDefined) {
+                throw new ModuleConfigException(__CLASS__, "The {$configKey} config path [{$path}] does not exist.");
+            }
+            $this->config[$configKey] = Utils::untrailslashit($absolutePath) . DIRECTORY_SEPARATOR;
         }
     }
 }
--- tests/unit/Codeception/Module/WPFilesystemTest.php
+++ tests/unit/Codeception/Module/WPFilesystemTest.php
@@ -5,6 +5,7 @@ namespace Codeception\Module;
 
 use Codeception\Exception\ModuleConfigException;
 use Codeception\Lib\ModuleContainer;
+use tad\WPBrowser\Filesystem\Utils;
 use function tad\WPBrowser\Tests\Support\rrmdir;
 
 class WPFilesystemTest extends \Codeception\Test\Unit {
@@ -89,6 +90,101 @@ class WPFilesystemTest extends \Codeception\Test\Unit {
         $this->expectException(ModuleConfigException::class);
 
         $this->make_instance();
+    }
+
+    /**
+     * It should only require the wpRootFolder path parameter and default the other parameters
+     *
+     * @test
+     */
+    public function it_should_only_require_the_wp_root_folder_path_parameter_and_default_the_other_parameters() {
+        $config = $this->getDefaultConfig();
+        $wpRoot = $config['wpRootFolder'];
+        $this->config = ['wpRootFolder' => $wpRoot];
+
+        $sut = $this->make_instance();
+        $sut->_initialize();
+
+        $moduleConfig = $sut->_getConfig();
+        $this->assertEquals(Utils::untrailslashit($wpRoot) . '/', $moduleConfig['wpRootFolder']);
+        $this->assertEquals($wpRoot . '/wp-content/themes/', $moduleConfig['themes']);
+        $this->assertEquals($wpRoot . '/wp-content/plugins/', $moduleConfig['plugins']);
+        $this->assertEquals($wpRoot . '/wp-content/mu-plugins/', $moduleConfig['mu-plugins']);
+        $this->assertEquals($wpRoot . '/wp-content/uploads/', $moduleConfig['uploads']);
+    }
+
+    /**
+     * It should allow passing optional parameters as relative paths
+     *
+     * @test
+     */
+    public function it_should_allow_passing_optional_parameters_as_relative_paths() {
+    }
+
+    public function optionalRequiredPathParameters() {
+        return [
+            ['themes'],
+            ['plugins'],
+            ['mu-plugins'],
+            ['uploads'],
+        ];
+    }
+
+    /**
+     * It should throw if optional parameter is specified but not existing
+     *
+     * @test
+     * @dataProvider optionalRequiredPathParameters
+     */
+    public function it_should_throw_if_optional_parameter_is_specified_but_not_existing($parameter) {
+        $config = $this->getDefaultConfig();
+
+        $this->config = [
+            'wpRootFolder' => $config['wpRootFolder'],
+            $parameter     => __DIR__ . '/foo',
+        ];
+
+        $this->expectException(ModuleConfigException::class);
+
+        $sut = $this->make_instance();
+        $sut->_initialize();
+    }
+
+    /**
+     * It should allow specifying wpRootFolder as relative path to the project root
+     *
+     * @test
+     */
+    public function it_should_allow_specifying_wp_root_folder_as_relative_path_to_the_project_root() {
+        $this->config = [
+            'wpRootFolder' => '/tests/_output/sandbox',
+        ];
+
+        $this->getDefaultConfig();
+        $sut = $this->make_instance();
+        $sut->_initialize();
+        $this->assertEquals(codecept_output_dir('sandbox/'), $sut->_getConfig('wpRootFolder'));
+    }
+
+    /**
+     * It should allow specifying optional path parameters as relative paths
+     *
+     * @test
+     * @dataProvider optionalRequiredPathParameters
+     */
+    public function it_should_allow_specifying_optional_path_parameters_as_relative_paths($parameter) {
+        $config = $this->getDefaultConfig();
+        $path = $config['wpRootFolder'] . '/foo/';
+        mkdir($path, 0777, true);
+
+        $this->config = [
+            'wpRootFolder' => $config['wpRootFolder'],
+            $parameter     => 'foo',
+        ];
+
+        $sut = $this->make_instance();
+        $sut->_initialize();
+        $this->assertEquals($path, $sut->_getConfig($parameter));
     }
 
     protected function _after() {
