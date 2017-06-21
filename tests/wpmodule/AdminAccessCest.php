c548bce4d85891c6a81883a543ee49c51d36687f
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-06-21 23:19:42 +0200

    change test order:

--- tests/wpmodule/AdminAccessCest.php
+++ tests/wpmodule/AdminAccessCest.php
@@ -5,6 +5,17 @@
  */
 class AdminAccessCest
 {
+  /**
+   * @test
+   * it should be able to log in and access the admin area
+   */
+  public function it_should_be_able_to_log_in_and_access_the_admin_area(WpmoduleTester $I)
+  {
+    $I->loginAsAdmin();
+    $I->amOnAdminPage('/');
+    $I->seeElement('body.index-php.wp-admin');
+  }
+
 	/**
 	 * @test
 	 * it should be able to visit the admin area when unlogged
@@ -13,16 +24,5 @@ class AdminAccessCest
 	{
 		$I->amOnAdminPage('/');
 		$I->seeElement('body.login');
-	}
-
-	/**
-	 * @test
-	 * it should be able to log in and access the admin area
-	 */
-	public function it_should_be_able_to_log_in_and_access_the_admin_area(WpmoduleTester $I)
-	{
-		$I->loginAsAdmin();
-		$I->amOnAdminPage('/');
-		$I->seeElement('body.index-php.wp-admin');
 	}
 }
