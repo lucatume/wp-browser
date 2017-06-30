c6615e106d3cb764b5df560ed2083121d991300d
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-06-30 18:13:23 +0200

    extend driver when needed'

--- src/Codeception/Lib/Driver/ExtendedMySql.php
+++ src/Codeception/Lib/Driver/ExtendedMySql.php
@@ -7,37 +7,39 @@ namespace Codeception\Lib\Driver;
  */
 class ExtendedMySql extends MySql
 {
-	/**
-	 * Returns the statement to insert or update an entry in the database.
-	 *
-	 * @param  string $tableName The table name to use
-	 * @param  array $data Key/value pairs of the data to insert/update.
-	 *
-	 * @return string            The query string ready to be prepared.
-	 */
-	public function insertOrUpdate($tableName, array $data)
-	{
-		$columns = array_map(
-			array($this, 'getQuotedName'),
-			array_keys($data)
-		);
-		$updateAssignments = $this->getAssignmentsFor($data);
-		return sprintf(
-			"INSERT INTO %s (%s) VALUES (%s) ON DUPLICATE KEY UPDATE %s",
-			$this->getQuotedName($tableName),
-			implode(', ', $columns),
-			implode(', ', array_fill(0, count($data), '?')),
-			$updateAssignments
-		);
-	}
+    /**
+     * Returns the statement to insert or update an entry in the database.
+     *
+     * @param  string $tableName The table name to use
+     * @param  array $data Key/value pairs of the data to insert/update.
+     *
+     * @return string            The query string ready to be prepared.
+     */
+    public function insertOrUpdate($tableName, array $data)
+    {
+        $this->executeQuery("SET SESSION sql_mode='ALLOW_INVALID_DATES'", []);
 
-	protected function getAssignmentsFor(array $data, $glue = ', ')
-	{
-		$assignments = array();
-		foreach ($data as $key => $value) {
-			$assignments[] = sprintf('%s=\'%s\'', $key, $value);
-		}
-		$assignments = implode($glue, $assignments);
-		return $assignments;
-	}
+        $columns = array_map(
+            array($this, 'getQuotedName'),
+            array_keys($data)
+        );
+        $updateAssignments = $this->getAssignmentsFor($data);
+        return sprintf(
+            "INSERT INTO %s (%s) VALUES (%s) ON DUPLICATE KEY UPDATE %s",
+            $this->getQuotedName($tableName),
+            implode(', ', $columns),
+            implode(', ', array_fill(0, count($data), '?')),
+            $updateAssignments
+        );
+    }
+
+    protected function getAssignmentsFor(array $data, $glue = ', ')
+    {
+        $assignments = array();
+        foreach ($data as $key => $value) {
+            $assignments[] = sprintf('%s=\'%s\'', $key, $value);
+        }
+        $assignments = implode($glue, $assignments);
+        return $assignments;
+    }
 }
