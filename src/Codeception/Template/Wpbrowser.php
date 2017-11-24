ec2d3e82ff04b114eb8ca5af7c58e2816f20670e
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-11-24 15:09:39 +0100

    make sure suites names are used to name the suites in init

--- src/Codeception/Template/Wpbrowser.php
+++ src/Codeception/Template/Wpbrowser.php
@@ -319,7 +319,7 @@ EOF;
             activatePlugins: [{$plugins}]
 EOF;
 
-		$this->createSuite('wpunit', $actor, $suiteConfig);
+		$this->createSuite($installationData['wpunitSuiteSlug'], $actor, $suiteConfig);
 	}
 
 	protected function createFunctionalSuite($actor = 'Functional', array $installationData = []) {
@@ -357,7 +357,7 @@ modules:
             adminPassword: '{$installationData['adminPassword']}'
             adminPath: '{$installationData['wpAdminPath']}'
 EOF;
-		$this->createSuite('functional', $actor, $suiteConfig);
+		$this->createSuite($installationData['functionalSuiteSlug'], $actor, $suiteConfig);
 	}
 
 	protected function createAcceptanceSuite($actor = 'Acceptance', array $installationData = null) {
@@ -395,7 +395,7 @@ modules:
             adminPassword: '{$installationData['adminPassword']}'
             adminPath: '{$installationData['wpAdminPath']}'
 EOF;
-		$this->createSuite('acceptance', $actor, $suiteConfig);
+		$this->createSuite($installationData['acceptanceSuiteSlug'], $actor, $suiteConfig);
 	}
 
 	protected function getDefaultInstallationData() {
