badf151ba45fde313c9be8764a649117396bda33
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-02-27 13:32:37 +0100

    add shims for PHPUnit classes used in Core test files

--- shims.php
+++ shims.php
@@ -4,6 +4,8 @@ namespace {
 
     // PHPUnit 6 compat
     if (class_exists('PHPUnit\Framework\TestCase')) {
+		class_alias('PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
+		class_alias('PHPUnit\Framework\Exception', 'PHPUnit_Framework_Exception');
         if ( ! class_exists('PHPUnit_Util_Getopt') && class_exists('PHPUnit\Util\Getopt')) {
             class_alias('PHPUnit\Util\Getopt', 'PHPUnit_Util_Getopt');
         }
