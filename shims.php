af17af390aaa101e0223c344c910c0e9ba633f51
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-02-27 14:01:59 +0100

    add the Assert shim

--- shims.php
+++ shims.php
@@ -8,6 +8,7 @@ namespace {
 			'PHPUnit\Framework\Test'                 => 'PHPUnit_Framework_Test',
 			'PHPUnit\Framework\TestSuite'            => 'PHPUnit_Framework_TestSuite',
 			'PHPUnit\Framework\TestCase'             => 'PHPUnit_Framework_TestCase',
+			'PHPUnit\Framework\Assert'               => 'PHPUnit_Framework_Assert',
 			'PHPUnit\Framework\Exception'            => 'PHPUnit_Framework_Exception',
 			'PHPUnit\Framework\Warning'              => 'PHPUnit_Framework_Warning',
 			'PHPUnit\Framework\TestListener'         => 'PHPUnit_Framework_TestListener',
