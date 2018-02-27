c890864e65b8c49d34c2ac2c3099ba27f2aded94
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-02-27 13:40:22 +0100

    add more aliases, reorganize shims

--- shims.php
+++ shims.php
@@ -4,10 +4,21 @@ namespace {
 
     // PHPUnit 6 compat
     if (class_exists('PHPUnit\Framework\TestCase')) {
-		class_alias('PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
-		class_alias('PHPUnit\Framework\Exception', 'PHPUnit_Framework_Exception');
-        if ( ! class_exists('PHPUnit_Util_Getopt') && class_exists('PHPUnit\Util\Getopt')) {
-            class_alias('PHPUnit\Util\Getopt', 'PHPUnit_Util_Getopt');
-        }
+		$aliases = [
+			'PHPUnit\Framework\Test'                 => 'PHPUnit_Framework_Test',
+			'PHPUnit\Framework\TestSuite'            => 'PHPUnit_Framework_TestSuite',
+			'PHPUnit\Framework\TestCase'             => 'PHPUnit_Framework_TestCase',
+			'PHPUnit\Framework\Exception'            => 'PHPUnit_Framework_Exception',
+			'PHPUnit\Framework\Warning'              => 'PHPUnit_Framework_Warning',
+			'PHPUnit\Framework\TestListener'         => 'PHPUnit_Framework_TestListener',
+			'PHPUnit\Framework\AssertionFailedError' => 'PHPUnit_Framework_AssertionFailedError',
+			'PHPUnit\Util\Getopt'                    => 'PHPUnit_Util_Getopt',
+		];
+
+		foreach ($aliases as $new => $old) {
+			if (!class_exists($old) && class_exists($new)) {
+				class_alias($new, $old);
+			}
+		}
     }
 }
