55abaa771155fb807e5a774f00022d0a9eb33348
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-03-02 19:07:14 +0100

    always define inverse aliases

--- shims.php
+++ shims.php
@@ -6,16 +6,22 @@ namespace {
 	if (class_exists('PHPUnit\Framework\TestCase')) {
 		$aliases = [
 			'PHPUnit\Util\Getopt' => 'PHPUnit_Util_Getopt',
-			// inverse aliases
-			'PHPUnit_Runner_Version' => 'PHPUnit\Runner\Version',
-			'PHPUnit_Framework_TestResult' => 'PHPUnit\Framework\TestResult',
-			'PHPUnit_Framework_Test' => 'PHPUnit\Framework\Test',
 		];
 
 		foreach ($aliases as $original => $alias) {
 			if (!class_exists($alias) && class_exists($original)) {
 				class_alias($original, $alias);
 			}
+		}
+
+		$inverseAliases = [
+			'PHPUnit_Runner_Version' => 'PHPUnit\Runner\Version',
+			'PHPUnit_Framework_TestResult' => 'PHPUnit\Framework\TestResult',
+			'PHPUnit_Framework_Test' => 'PHPUnit\Framework\Test',
+		];
+
+		foreach ($inverseAliases as $original => $alias) {
+			class_alias($original, $alias);
 		}
 	}
 }
