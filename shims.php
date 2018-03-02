d56dc635baaf2e6e51e93d6931b413502518c2f3
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-03-02 03:32:58 +0100

    add `Assert` alias

--- shims.php
+++ shims.php
@@ -4,8 +4,33 @@ namespace {
 
 	// PHPUnit 6 compat
 	if (class_exists('PHPUnit\Framework\TestCase')) {
-		if ( ! class_exists('PHPUnit_Util_Getopt') && class_exists('PHPUnit\Util\Getopt')) {
-			class_alias('PHPUnit\Util\Getopt', 'PHPUnit_Util_Getopt');
+		$aliases = [
+			//			'PHPUnit\Framework\Test'                       => 'PHPUnit_Framework_Test',
+			//			'PHPUnit\Framework\TestSuite'                  => 'PHPUnit_Framework_TestSuite',
+			//			'PHPUnit\Framework\TestCase'                   => 'PHPUnit_Framework_TestCase',
+			'PHPUnit\Framework\Assert' => 'PHPUnit_Framework_Assert',
+			//			'PHPUnit\Framework\Exception'                  => 'PHPUnit_Framework_Exception',
+			//			'PHPUnit\Framework\ExpectationFailedException' => 'PHPUnit_Framework_ExpectationFailedException',
+			//			'PHPUnit\Framework\Warning'                    => 'PHPUnit_Framework_Warning',
+			//			'PHPUnit\Framework\Error\Notice'               => 'PHPUnit_Framework_Error_Notice',
+			//			'PHPUnit\Framework\Error\Warning'              => 'PHPUnit_Framework_Error_Warning',
+			//			'PHPUnit\Framework\TestListener'               => 'PHPUnit_Framework_TestListener',
+			//			'PHPUnit\Framework\AssertionFailedError'       => 'PHPUnit_Framework_AssertionFailedError',
+			'PHPUnit\Util\Getopt'      => 'PHPUnit_Util_Getopt',
+			//			'PHPUnit\Util\GlobalState'                     => 'PHPUnit_Util_GlobalState',
+		];
+
+		foreach ($aliases as $new => $old) {
+			if (!class_exists($old) && class_exists($new)) {
+				class_alias($new, $old);
+			}
+			// PHPUnit 6 compat
+			if (class_exists('PHPUnit\Framework\TestCase')) {
+				if (!class_exists('PHPUnit_Util_Getopt') && class_exists('PHPUnit\Util\Getopt')) {
+					class_alias('PHPUnit\Util\Getopt', 'PHPUnit_Util_Getopt');
+				}
+			}
 		}
 	}
 }
+
