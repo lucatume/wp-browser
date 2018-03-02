d28df2fe56c8fd1aedafa20e5c36e28ed60573be
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-03-02 15:02:25 +0100

    alias the Runner\Version class

--- shims.php
+++ shims.php
@@ -2,10 +2,18 @@
 
 namespace {
 
-    // PHPUnit 6 compat
-    if (class_exists('PHPUnit\Framework\TestCase')) {
-        if ( ! class_exists('PHPUnit_Util_Getopt') && class_exists('PHPUnit\Util\Getopt')) {
-            class_alias('PHPUnit\Util\Getopt', 'PHPUnit_Util_Getopt');
-        }
-    }
+	// PHPUnit 6+ compat
+	if (class_exists('PHPUnit\Framework\TestCase')) {
+		$aliases = [
+			'PHPUnit\Util\Getopt' => 'PHPUnit_Util_Getopt',
+			// inverse aliases
+			'PHPUnit_Runner_Version' => 'PHPUnit\Runner\Version',
+		];
+
+		foreach ($aliases as $original => $alias) {
+			if (!class_exists($alias) && class_exists($original)) {
+				class_alias($original, $alias);
+			}
+		}
+	}
 }
