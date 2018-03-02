e1ebcb125e984360460bf49e170612ef29cd2cff
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-03-02 04:24:30 +0100

    avoid autoloading the test cases

--- shims.php
+++ shims.php
@@ -2,20 +2,11 @@
 
 namespace {
 
+	// PHPUnit 6+ compat
 	if (class_exists('PHPUnit\Framework\TestCase')) {
 		$aliases = [
-			// PHPUnit 6+ compat
 			'PHPUnit\Framework\Assert' => 'PHPUnit_Framework_Assert',
 			'PHPUnit\Util\Getopt' => 'PHPUnit_Util_Getopt',
-
-			// Core test case classes
-			'Codeception\TestCase\WpTestCase' => 'WP_UnitTestCase',
-			'Codeception\TestCase\WPRestApiTestCase' => 'WP_Test_REST_TestCase',
-			'Codeception\TestCase\WPXMLRPCTestCase' => 'WP_XMLRPC_UnitTestCase',
-			'Codeception\TestCase\WPAjaxTestCase' => 'WP_Ajax_UnitTestCase',
-			'Codeception\TestCase\WPCanonicalTestCase' => 'WP_Canonical_UnitTestCase',
-			'Codeception\TestCase\WPRestControllerTestCase' => 'WP_Test_REST_Controller_Testcase',
-			'Codeception\TestCase\WPRestPostTypeControllerTestCase' => 'WP_Test_REST_Post_Type_Controller_Testcase',
 		];
 
 		foreach ($aliases as $new => $old) {
@@ -23,5 +14,19 @@ namespace {
 				class_alias($new, $old);
 			}
 		}
+	}
+
+	$testcaseAliases = [
+		'Codeception\TestCase\WpTestCase' => 'WP_UnitTestCase',
+		'Codeception\TestCase\WPRestApiTestCase' => 'WP_Test_REST_TestCase',
+		'Codeception\TestCase\WPXMLRPCTestCase' => 'WP_XMLRPC_UnitTestCase',
+		'Codeception\TestCase\WPAjaxTestCase' => 'WP_Ajax_UnitTestCase',
+		'Codeception\TestCase\WPCanonicalTestCase' => 'WP_Canonical_UnitTestCase',
+		'Codeception\TestCase\WPRestControllerTestCase' => 'WP_Test_REST_Controller_Testcase',
+		'Codeception\TestCase\WPRestPostTypeControllerTestCase' => 'WP_Test_REST_Post_Type_Controller_Testcase',
+	];
+
+	foreach ($testcaseAliases as $old => $new) {
+		class_alias($new, $old);
 	}
 }
