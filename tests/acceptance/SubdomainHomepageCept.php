5717cbd6c699cf0ef02bdbecb94717e24a739af4
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2018-11-24 12:32:10 +0100

    use the correct sub-domain name

--- tests/acceptance/SubdomainHomepageCept.php
+++ tests/acceptance/SubdomainHomepageCept.php
@@ -5,7 +5,7 @@ $I->amOnSubdomain(getenv('WP_SUBDOMAIN_1') ?: 'test1');
 $I->amOnPage('/');
 $I->seeElement('body.home');
 $I->seeInTitle(getenv('WP_SUBDOMAIN_1_TITLE') ?: 'Test Subdomain 1');
-$I->amOnSubdomain(getenv('WP_SUBDOMAIN_1') ?: 'test2');
+$I->amOnSubdomain(getenv('WP_SUBDOMAIN_2') ?: 'test2');
 $I->amOnPage('/');
 $I->seeElement('body.home');
 $I->seeInTitle(getenv('WP_SUBDOMAIN_2_TITLE') ?: 'Test Subdomain 2');
