66d514e41720802db8fc0edecf97198999f601af
Luca Tumedei <luca@theaveragedev.com>
2019-09-03 10:21:37 +0200

    feat(functions) add utils and tests

---
+++ src/tad/WPBrowser/utils.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Miscellaneous utility functions for the wp-browser library.
+ *
+ * @package tad\WPBrowser
+ */
+
+namespace tad\WPBrowser;
+
+/**
+ * Builds an array format command line, compatible with the Symfony Process component, from a string command line.
+ *
+ * @param string|array $command The command line to parse, if in array format it will not be modified.
+ *
+ * @return array The parsed command line, in array format. Untouched if originally already an array.
+ *
+ * @uses \Symfony\Component\Process\Process To parse and escape the command line.
+ */
+function buildCommandline($command)
+{
+    if (empty($command)|| is_array($command)) {
+        return array_filter((array)$command);
+    }
+
+    $escapedCommandLine = ( new \Symfony\Component\Process\Process($command) )->getCommandLine();
+    $commandLineFrags   = explode(' ', $escapedCommandLine);
+
+    if (count($commandLineFrags) === 1) {
+        return $commandLineFrags;
+    }
+
+    $open = false;
+	$unescapedQuotesPattern  = '/(?<!\\\\)"/u';
+
+	return array_reduce($commandLineFrags, static function (array $acc, $v) use (&$open, $unescapedQuotesPattern) {
+        $containsUnescapedQuotes = preg_match($unescapedQuotesPattern, $v);
+        $v                       = $open && $containsUnescapedQuotes ? array_pop($acc) . ' ' . $v : $v;
+        $open                    = ! $open && $containsUnescapedQuotes;
+        $acc[]                   = preg_replace($unescapedQuotesPattern, '', $v);
+
+        return $acc;
+    }, []);
+}
---
+++ tests/unit/tad/WPBrowser/utilsTest.php
@@ -0,0 +1,43 @@
+<?php namespace tad\WPBrowser;
+
+class utilsTest extends \Codeception\Test\Unit
+{
+    /**
+     * @dataProvider buildCommandlineDataProvider
+     */
+    public function test_build_commandline($input, $expected)
+    {
+        $this->assertEquals($expected, buildCommandline($input));
+    }
+
+    public function buildCommandlineDataProvider()
+    {
+        return [
+            'empty_string_input' => ['',[]],
+            'array_input'              => [
+                [ 'core', 'version', '--allow-root' ],
+                [ 'core', 'version', '--allow-root' ]
+            ],
+            'one_word_string'          => [ 'core', [ 'core' ] ],
+            'one_option_string'        => [ '--help', [ '--help' ] ],
+            'string_w_flag'            => [ 'core version --allow-root', [ 'core', 'version', '--allow-root' ] ],
+            'string_w_3_flags'         => [
+                'core version --allow-root --flag_2 --flag_3',
+                [ 'core', 'version', '--allow-root', '--flag_2','--flag_3' ]
+            ],
+            'string_w_unquoted_option' => [ 'core version --opt_1=foo', [ 'core', 'version', '--opt_1=foo' ] ],
+            'string_w_quoted_option'   => [
+                'core version --opt_1="foo bar"',
+                [ 'core', 'version', '--opt_1=foo bar' ]
+            ],
+            'string_input_mix'           => [
+                'some command --opt_1="/var/www/html" --flag_2 --opt_2="Happy \"Quotes\""',
+                [ 'some', 'command', '--opt_1=/var/www/html', '--flag_2', '--opt_2=Happy \"Quotes\"' ]
+            ],
+            'case_1'=>[
+            'post create --post_title="Some Post" --post_type=post' ,
+               ['post','create','--post_title=Some Post','--post_type=post']
+            ]
+        ];
+    }
+}
