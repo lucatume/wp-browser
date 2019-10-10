e99542f844c53cf729f52d0e98a0b42f1b682fd7
Timothy Jacobs <timothy@ironbounddesigns.com>
2019-10-10 15:12:41 -0400

    Move CLI tests to climodule. Fix regex.

--- tests/climodule/cli/SeeMethodsCest.php
+++ tests/climodule/cli/SeeMethodsCest.php
@@ -1,32 +1,33 @@
 <?php
+use ClimoduleTester as Tester;
 
 class SeeMethodsCest
 {
-    public function it_should_see_correct_output(Wpcli_moduleTester $I)
+    public function it_should_see_correct_output(Tester $I)
     {
         $I->cli('eval "echo \'hi\';"');
         $I->seeInShellOutput( 'hi' );
     }
 
-    public function it_should_not_see_correct_output(Wpcli_moduleTester $I)
+    public function it_should_not_see_correct_output(Tester $I)
     {
         $I->cli('eval "echo \'hi\';"');
         $I->dontSeeInShellOutput( 'hello' );
     }
 
-    public function it_should_see_correct_output_matches(Wpcli_moduleTester $I)
+    public function it_should_see_correct_output_matches(Tester $I)
     {
         $I->cli('eval "echo \'hi\';"');
-        $I->seeShellOutputMatches( '\w+' );
+        $I->seeShellOutputMatches( '/\w+/' );
     }
 
-    public function it_should_see_correct_result_code(Wpcli_moduleTester $I)
+    public function it_should_see_correct_result_code(Tester $I)
     {
         $I->cli('eval "echo \'hi\';"');
         $I->seeResultCodeIs(0);
     }
 
-    public function it_should_not_see_correct_result_code(Wpcli_moduleTester $I)
+    public function it_should_not_see_correct_result_code(Tester $I)
     {
         $I->cli('eval "echo \'hi\';"');
         $I->seeResultCodeIsNot(1);
