9709c0d8cd3447a5d1f17b9c484115bbdba89e96
Luca Tumedei <luca@theaveragedev.com>
2020-03-09 19:51:34 +0100

    fix(_support/functions) add and more support functions

--- tests/_support/functions.php
+++ tests/_support/functions.php
@@ -2,9 +2,12 @@
 
 namespace tad\WPBrowser\Tests\Support;
 
+use Dotenv\Environment\DotenvFactory;
+use Dotenv\Loader;
+
 function importDump($dumpFile, $dbName, $dbUser = 'root', $dbPass = 'root', $dbHost = 'localhost')
 {
-    if (strpos($dbHost, ':') >0) {
+    if (strpos($dbHost, ':') > 0) {
         list($dbHost, $dbPort) = explode(':', $dbHost);
         $dbHost = sprintf('%s -P %d', $dbHost, $dbPort);
     }
@@ -24,14 +27,63 @@ function importDump($dumpFile, $dbName, $dbUser = 'root', $dbPass = 'root', $dbH
     return (int)$status === 0;
 }
 
-
 /**
-* Normalizes a string new line bytecode for comparison
-* through Unix and Windows environments.
-*
-* @see https://stackoverflow.com/a/7836692/2056484
-*/
+ * Normalizes a string new line bytecode for comparison
+ * through Unix and Windows environments.
+ *
+ * @see https://stackoverflow.com/a/7836692/2056484
+ */
 function normalizeNewLine($str)
 {
     return preg_replace('~(*BSR_ANYCRLF)\R~', "\n", $str);
+}
+
+/**
+ * Open a database connection and returns a callable to run queries on it.
+ *
+ * @param string $host The database host.
+ * @param string $user The database user.
+ * @param string $pass The database password.
+ * @return \Closure A callable to run queries on the database.
+ */
+function db($host, $user, $pass)
+{
+    $pdo = new \PDO("mysql:host={$host}", $user, $pass);
+    return static function ($query) use ($pdo, $host, $user, $pass) {
+        $result = $pdo->exec($query);
+        if (false === $result) {
+            throw new \RuntimeException('Could not create the subdir db; ' . json_encode([
+                    'host' => $host,
+                    'user' => $user,
+                    'pass' => $pass,
+                    'query' => $query
+                ], JSON_PRETTY_PRINT));
+        }
+
+        return $result;
+    };
+}
+
+/**
+ * Returns the name of the environment file to load in tests.
+ *
+ * @return string The name of the environment file to load in tests.
+ */
+function envFile(){
+   return '.env.testing.docker';
+}
+
+/**
+ * Returns a closure to get the value of an environment variable, loading a specific env file first.
+ *
+ * @param string|null $file The name of the environment file to load, or `null` to use the default one.
+ * @return \Closure A closure taking one argument, the environment variable name, to return it.
+ */
+function env($file = null)
+{
+    $envFile = $file ?: envFile();
+    $env = new Loader([codecept_root_dir($envFile)], new DotenvFactory());
+    return static function ($name) use ($env) {
+        return $env->getEnvironmentVariable($name);
+    };
 }
