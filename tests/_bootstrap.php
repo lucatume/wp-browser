ca11dccb93b7224e07252a25eede574c15dac2f3
Luca Tumedei <luca@theaveragedev.com>
2020-03-13 10:29:46 +0100

    test(_bootstrap.php) if uopx ext active, allow exit and die

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,22 +1,10 @@
 <?php
 // This is global bootstrap for autoloading.
+use function tad\WPBrowser\Tests\Support\createTestDatabasesIfNotExist;
 
-use function tad\WPBrowser\Tests\Support\db;
-use function tad\WPBrowser\Tests\Support\env;
+createTestDatabasesIfNotExist();
 
-$env = env();
-$host = $env('WORDPRESS_DB_HOST');
-$user = $env('WORDPRESS_DB_USER');
-$pass = $env('WORDPRESS_DB_PASSWORD');
-$subfolderDb = $env('WORDPRESS_SUBDIR_DB_NAME');
-$subdomainDb = $env('WORDPRESS_SUBDOMAIN_DB_NAME');
-$emptyDb = $env('WORDPRESS_EMPTY_DB_NAME');
-$db = db($host, $user, $pass);
-$db("CREATE DATABASE IF NOT EXISTS {$subfolderDb}");
-$db("CREATE DATABASE IF NOT EXISTS {$subdomainDb}");
-$db("CREATE DATABASE IF NOT EXISTS {$emptyDb}");
-
-// If the `uopz` extension is installed, then allow `exit` and `die` to work normally.
+// If the `uopz` extension is installed, then ensure `exit` and `die` to work normally.
 if (function_exists('uopz_allow_exit')) {
     uopz_allow_exit(true);
 }
