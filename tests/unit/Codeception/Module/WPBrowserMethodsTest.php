9b9009bd9c3f18f130e559ed060328cf28f1cafb
Luca Tumedei <luca@theaveragedev.com>
2020-05-11 17:13:04 +0200

    fix(WPBrowserMethods) cron page path building

--- tests/unit/Codeception/Module/WPBrowserMethodsTest.php
+++ tests/unit/Codeception/Module/WPBrowserMethodsTest.php
@@ -5,8 +5,30 @@ namespace Codeception\Module;
 class WPBrowserMethodsTest extends \Codeception\Test\Unit
 {
     use WPBrowserMethods;
-   
+
     protected $page;
+
+    public static function cronPageDataProvider()
+    {
+        return [
+            'one element array' => [
+                ['action' => 'foo_action'],
+                'wp-cron.php?action=foo_action'
+            ],
+            'multi element array' => [
+                ['action' => 'foo_action', 'data' => 'bar_data', 'nonce' => 'baz_nonce'],
+                'wp-cron.php?action=foo_action&data=bar_data&nonce=baz_nonce'
+            ],
+            'string' => [
+                'action=foo_action&data=bar_data&nonce=baz_nonce',
+                'wp-cron.php?action=foo_action&data=bar_data&nonce=baz_nonce'
+            ],
+            'string w/ question mark' => [
+                '?action=foo_action&data=bar_data&nonce=baz_nonce',
+                'wp-cron.php?action=foo_action&data=bar_data&nonce=baz_nonce'
+            ]
+        ];
+    }
 
     public function adminAjaxPageDatqProvider()
     {
@@ -42,6 +64,19 @@ class WPBrowserMethodsTest extends \Codeception\Test\Unit
     public function it_should_point_to_ajax_file_when_requesting_ajax_page_with_query_vars($input, $expected)
     {
         $this->amOnAdminAjaxPage($input);
+
+        $this->assertEquals($expected, $this->page);
+    }
+
+    /**
+     * @test
+     * It should point to cron file when requesting cron page with query vars
+     *
+     * @dataProvider cronPageDataProvider
+     */
+    public function it_should_point_to_cron_file_when_requesting_cron_page_with_query_vars($input, $expected)
+    {
+        $this->amOnCronPage($input);
 
         $this->assertEquals($expected, $this->page);
     }
