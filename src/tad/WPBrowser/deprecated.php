6179ccb06c829f566e1049dab0b78e0f7549f4a7
Luca Tumedei <luca@theaveragedev.com>
2020-05-13 09:59:27 +0200

    style(src/deprecated.php) apply PSR-2 standards to code

--- src/tad/WPBrowser/deprecated.php
+++ src/tad/WPBrowser/deprecated.php
@@ -5,17 +5,18 @@
  * @package tad\WPBrowser
  */
 
-if ( ! function_exists( 'rrmdir' ) ) {
-	/**
-	 * Recursively removes a directory and all its content.
-	 *
-	 * @see        tad\WPBrowser\rrmdir() for the replacement function.
-	 * @deprecated Since 2.3; moved to the `\tad\WPBrowser` namespace.
-	 *
-	 * @param string $src The absolute path to the directory to remove.
-	 *
-	 */
-	function rrmdir( $src ) {
-		tad\WPBrowser\rrmdir( $src );
-	}
+if (! function_exists('rrmdir')) {
+    /**
+     * Recursively removes a directory and all its content.
+     *
+     * @see        tad\WPBrowser\rrmdir() for the replacement function.
+     * @deprecated Since 2.3; moved to the `\tad\WPBrowser` namespace.
+     *
+     * @param string $src The absolute path to the directory to remove.
+     *
+     */
+    function rrmdir($src)
+    {
+        tad\WPBrowser\rrmdir($src);
+    }
 }
