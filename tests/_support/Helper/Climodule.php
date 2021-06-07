07723e12bee16d6dba4dad48c5c8bbdd7fcc3130
Luca Tumedei <luca@theaveragedev.com>
2021-06-07 16:50:54 +0200

    refactor(Helper/Climodule) better catch

--- tests/_support/Helper/Climodule.php
+++ tests/_support/Helper/Climodule.php
@@ -4,17 +4,24 @@ namespace Helper;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
-class Climodule extends \Codeception\Module
+use Codeception\Module;
+use tad\WPBrowser\Exceptions\WpCliException;
+
+class Climodule extends Module
 {
     public function setCliEnv($key, $value)
     {
-        /** @var \Codeception\Module\WPCLI $cli */
-        $cli = $this->getModule('WPCLI');
-        $cliConfig = $cli->_getConfig();
-        $cliConfig['env'] = isset($cliConfig['env']) ?
-            array_merge($cliConfig['env'], [$key => $value])
-            : [$key => $value];
+        try {
+            /** @var \Codeception\Module\WPCLI $cli */
+            $cli = $this->getModule('WPCLI');
+            $cliConfig = $cli->_getConfig();
+            $cliConfig['env'] = isset($cliConfig['env']) ?
+                array_merge($cliConfig['env'], [$key => $value])
+                : [$key => $value];
 
-        $cli->_reconfigure($cliConfig);
+            $cli->_reconfigure($cliConfig);
+        } catch (\Exception $e) {
+            throw new WpCliException($e->getMessage(), $e->getCode(), $e);
+        }
     }
 }
