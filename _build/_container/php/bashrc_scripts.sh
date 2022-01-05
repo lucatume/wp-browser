73d220974b2bf21c5fa9a8e4413f8c4c9952aef0
Luca Tumedei <luca@theaveragedev.com>
2022-01-05 23:19:15 +0100

    build(_build/_container/php) better xon/xoff scripts

--- _build/_container/php/bashrc_scripts.sh
+++ _build/_container/php/bashrc_scripts.sh
@@ -1,17 +1,18 @@
 alias c="vendor/bin/codecept"
 alias cr="vendor/bin/codecept run"
+
+function xdebug_config_file(){
+  echo "$(php --ini | grep xdebug | cut -d, -f1)"
+}
+
 function xon(){
-  xdebug_config_file="$(php --ini | grep xdebug)"
-  sed  '/^;zend_extension/ s/;zend_extension/zend_extension/g' "$(php --ini | grep xdebug)" > xdebug_config.tmp
-  mv xdebug_config.tmp "$xdebug_config_file"
-  rm -f xdebug_config.tmp
+  xdebug_config_file=$(xdebug_config_file)
+  sed -i '/^;zend_extension/ s/;zend_extension/zend_extension/g' "$xdebug_config_file"
   php -v
 }
 function xoff(){
-  xdebug_config_file="$(php --ini | grep xdebug)"
-  sed  '/^zend_extension/ s/zend_extension/;zend_extension/g' "$(php --ini | grep xdebug)" > xdebug_config.tmp
-  mv xdebug_config.tmp "$xdebug_config_file"
-  rm -f xdebug_config.tmp
+  xdebug_config_file=$(xdebug_config_file)
+  sed -i '/^zend_extension/ s/zend_extension/;zend_extension/g' "$xdebug_config_file"
   php -v
 }
 
