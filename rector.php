d0a954fd20a296cbb44e1bd194eae711f93d5dd1
Luca Tumedei <luca@theaveragedev.com>
2022-08-02 10:33:24 +0200

    build(rector.php) add more rules

--- rector.php
+++ rector.php
@@ -3,9 +3,19 @@
 declare( strict_types=1 );
 
 use Rector\CodeQuality\Rector\ClassMethod\ReturnTypeFromStrictScalarReturnExprRector;
+use Rector\CodeQuality\Rector\If_\SimplifyIfIssetToNullCoalescingRector;
+use Rector\CodeQuality\Rector\If_\SimplifyIfNotNullReturnRector;
 use Rector\Config\RectorConfig;
+use Rector\PostRector\Rector\NameImportingPostRector;
+use Rector\PostRector\Rector\UseAddingPostRector;
 use Rector\Set\ValueObject\SetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\AddReturnTypeDeclarationBasedOnParentClassMethodRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromReturnNewRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictBoolReturnExprRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictNativeCallRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictNewArrayRector;
+use Rector\TypeDeclaration\Rector\ClassMethod\ReturnTypeFromStrictTypedPropertyRector;
 use Rector\TypeDeclaration\Rector\Closure\AddClosureReturnTypeRector;
 
 return static function ( RectorConfig $rectorConfig ): void {
@@ -17,4 +27,15 @@ return static function ( RectorConfig $rectorConfig ): void {
 	$rectorConfig->rule(ReturnTypeFromStrictScalarReturnExprRector::class);
 	$rectorConfig->rule(AddClosureReturnTypeRector::class);
 	$rectorConfig->rule(AddReturnTypeDeclarationBasedOnParentClassMethodRector::class);
+    $rectorConfig->rule(AddVoidReturnTypeWhereNoReturnRector::class);
+    $rectorConfig->rule(ReturnTypeFromReturnNewRector::class);
+    $rectorConfig->rule(ReturnTypeFromStrictBoolReturnExprRector::class);
+    $rectorConfig->rule(ReturnTypeFromStrictNativeCallRector::class);
+    $rectorConfig->rule(ReturnTypeFromStrictNewArrayRector::class);
+    $rectorConfig->rule(ReturnTypeFromStrictTypedPropertyRector::class);
+    $rectorConfig->rule(ReturnTypeFromStrictScalarReturnExprRector::class);
+    $rectorConfig->rule(SimplifyIfIssetToNullCoalescingRector::class);
+    $rectorConfig->rule(SimplifyIfNotNullReturnRector::class);
+    $rectorConfig->rule(NameImportingPostRector::class);
+    $rectorConfig->rule(UseAddingPostRector::class);
 };
