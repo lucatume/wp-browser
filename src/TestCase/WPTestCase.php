64aad760d5dd315dee7bdd000fdb5daeac9dcd97
Luca Tumedei <luca@theaveragedev.com>
2022-08-27 17:35:26 +0200

    refactor(TestCase/WPTestCase) rewrite Unit + Core

--- src/TestCase/WPTestCase.php
+++ src/TestCase/WPTestCase.php
@@ -1,22 +1,116 @@
 <?php
-/**
- * ${CARET}
- *
- * @since   TBD
- *
- * @package lucatume\WPBrowser\TestCase;
- */
 
 namespace lucatume\WPBrowser\TestCase;
 
-/**
- * Class WPTestCase.
- *
- * @since   TBD
- *
- * @package lucatume\WPBrowser\TestCase;
- */
-class WPTestCase
+use Codeception\Test\Unit;
+use ReflectionMethod;
+use WP_UnitTestCase;
+
+class WPTestCase extends Unit
 {
+    // Backup, and reset, globals between tests.
+    protected $backupGlobals = true;
+
+    // A list of globals that should not be backed up, they are handled by the Core test case.
+    protected $backupGlobalsExcludeList = [
+        'wpdb',
+        'wp_query',
+        'wp',
+        'post',
+        'id',
+        'authordata',
+        'currentday',
+        'currentmonth',
+        'page',
+        'pages',
+        'multipage',
+        'more',
+        'numpages',
+        'current_screen',
+        'taxnow',
+        'typenow',
+        'wp_actions',
+        'wp_current_filter',
+        'wp_filter',
+        'wp_object_cache',
+        'wp_meta_keys'
+    ];
+
+    // Backup, and reset, static class attributes between tests.
+    protected $backupStaticAttributes = true;
+
+    /**
+     * @var array<string,WP_UnitTestCase>
+     */
+    private static array $coreTestCaseMap = [];
+
+    private static function getCoreTestCase(): WP_UnitTestCase
+    {
+        if (isset(self::$coreTestCaseMap[static::class])) {
+            return self::$coreTestCaseMap[static::class];
+        }
+
+        $coreTestCase = new class extends WP_UnitTestCase {
+        };
+        $coreTestCase->setCalledClass(static::class);
+        self::$coreTestCaseMap[static::class] = $coreTestCase;
+
+        return $coreTestCase;
+    }
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+        self::getCoreTestCase()->set_up_before_class();
+    }
+
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        self::getCoreTestCase()->set_up();
+    }
+
+    protected function tearDown(): void
+    {
+        self::getCoreTestCase()->tear_down();
+        parent::tearDown();
+    }
+
+
+    public static function tearDownAfterClass(): void
+    {
+        self::getCoreTestCase()->tear_down_after_class();
+        parent::tearDownAfterClass();
+    }
+
+    protected function assertPostConditions(): void
+    {
+        parent::assertPostConditions();
+        self::getCoreTestCase()->assert_post_conditions();
+    }
+
+    public function __destruct()
+    {
+        // Allow garbage collection of the core test case instance.
+        unset(self::$coreTestCaseMap[static::class]);
+    }
+
+    public static function __callStatic(string $name, array $arguments)
+    {
+        $coreTestCase = self::getCoreTestCase();
+        $reflectionMethod = new ReflectionMethod($coreTestCase, $name);
+        $reflectionMethod->setAccessible(true);
+        return $reflectionMethod->invokeArgs(null, $arguments);
+    }
+
+    public function __call(string $name, array $arguments)
+    {
+        $coreTestCase = self::getCoreTestCase();
+        $reflectionMethod = new ReflectionMethod($coreTestCase, $name);
+        $reflectionMethod->setAccessible(true);
+        return $reflectionMethod->invokeArgs($coreTestCase, $arguments);
+    }
+
 
 }
