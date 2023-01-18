50c0b19b14e77bc2b3806ce133c451838806a1c0
Luca Tumedei <luca@theaveragedev.com>
2023-01-18 19:24:31 +0100

    test(unit) update Copier extension tests to use real FS

--- tests/unit/lucatume/WPBrowser/Extension/CopierTest.php
+++ tests/unit/lucatume/WPBrowser/Extension/CopierTest.php
@@ -1,14 +1,17 @@
 <?php
+
 namespace lucatume\WPBrowser\Extension;
 
 use Codeception\Event\SuiteEvent;
 use Codeception\Exception\ExtensionException;
-use lucatume\WPBrowser\Extension\Copier;
+use lucatume\WPBrowser\Tests\Traits\UopzFunctions;
 use lucatume\WPBrowser\Utils\Filesystem as FS;
-use org\bovigo\vfs\vfsStream;
+
 
 class CopierTest extends \Codeception\Test\Unit
 {
+    use UopzFunctions;
+
     protected $backupGlobals = false;
     /**
      * @var \UnitTester
@@ -34,9 +37,9 @@ class CopierTest extends \Codeception\Test\Unit
      * @test
      * it should be instantiatable
      */
-    public function it_should_be_instantiatable()
+    public function it_should_be_instantiatable(): void
     {
-        $sut = $this->make_instance();
+        $sut = $this->makeInstance();
 
         $this->assertInstanceOf(Copier::class, $sut);
     }
@@ -44,7 +47,7 @@ class CopierTest extends \Codeception\Test\Unit
     /**
      * @return Copier
      */
-    private function make_instance()
+    private function makeInstance(): Copier
     {
         return new Copier($this->config, $this->options);
     }
@@ -53,7 +56,7 @@ class CopierTest extends \Codeception\Test\Unit
      * @test
      * it should throw if source file does not exist
      */
-    public function it_should_throw_if_source_file_does_not_exist()
+    public function it_should_throw_if_source_file_does_not_exist(): void
     {
         $this->config = [
             'files' => [
@@ -64,92 +67,97 @@ class CopierTest extends \Codeception\Test\Unit
         $this->expectException(ExtensionException::class);
 
 
-        $this->make_instance();
+        $this->makeInstance();
     }
 
     /**
      * @test
      * it should throw if source file is not readable
      */
-    public function it_should_throw_if_source_file_is_not_readable()
+    public function it_should_throw_if_source_file_is_not_readable(): void
     {
-        $root = vfsStream::setup();
-        $root->addChild(vfsStream::newFile('some-file', 0000));
+        $root = FS::tmpDir('copier_', [
+            'some-file' => 'some content'
+        ]);
+        $filePath = $root . '/some-file';
+        $this->uopzSetFunctionReturn('is_readable', static function (string $file) use ($filePath) {
+            return $file !== $filePath && is_readable($file);
+        }, true);
         $this->config = [
             'files' => [
-                $root->url() . '/some-file' => __DIR__ . '/two'
+                $filePath => __DIR__ . '/two'
             ]
         ];
 
         $this->expectException(ExtensionException::class);
 
-
-        $this->make_instance();
+        $this->makeInstance();
     }
 
     /**
      * @test
      * it should throw if destination folder does not exist
      */
-    public function it_should_throw_if_destination_folder_does_not_exist()
+    public function it_should_throw_if_destination_folder_does_not_exist(): void
     {
-        $root = vfsStream::setup();
-        $root->addChild(vfsStream::newFile('some-file', 0777));
+        $root = FS::tmpDir('copier_', [
+            'some-file' => 'some content'
+        ]);
+        $filePath = $root . '/some-file';
         $this->config = [
             'files' => [
-                $root->url() . '/some-file' => $root->url() . '/some-folder/another-file'
+                $filePath => $root . '/some-folder/another-file'
             ]
         ];
 
         $this->expectException(ExtensionException::class);
 
-
-        $this->make_instance();
+        $this->makeInstance();
     }
 
     /**
      * @test
      * it should throw if destination folder is not writeable
      */
-    public function it_should_throw_if_destination_folder_is_not_writeable()
+    public function it_should_throw_if_destination_folder_is_not_writeable(): void
     {
-        $root = vfsStream::setup();
-        $root->addChild(vfsStream::newFile('some-file', 0777));
-        $root->addChild(vfsStream::newFile('another-file', 0777));
-        $destFolder = vfsStream::newDirectory('destination', 0000);
-        $root->addChild($destFolder);
+        $root = FS::tmpDir('copier_', [
+            'some-file' => 'some content',
+            'some-other-file' => 'some other content',
+            'destination-dir' => []
+        ]);
+        $this->uopzSetFunctionReturn('is_writable', static function (string $file) use ($root) {
+            return $file !== $root . '/destination-dir' && is_writable($file);
+        }, true);
         $this->config = [
             'files' => [
-                $root->url() . '/some-file' => $root->url() . '/destination/some-file'
+                $root . '/some-file' => $root . '/destination-dir/some-file'
             ]
         ];
 
         $this->expectException(ExtensionException::class);
 
-
-        $this->make_instance();
+        $this->makeInstance();
     }
 
     /**
      * @test
      * it should write source file to destination file
      */
-    public function it_should_write_source_file_to_destination_file()
+    public function it_should_write_source_file_to_destination_file(): void
     {
-        $root = vfsStream::setup();
-        $file = vfsStream::newFile('some-file', 0777);
-        $file->setContent('foo bar');
-        $root->addChild($file);
-        $destFolder = vfsStream::newDirectory('destination', 0777);
-        $root->addChild($destFolder);
-        $destinationFile = $root->url() . '/destination/some-file';
+        $root = FS::tmpDir('copier_', [
+            'some-file' => 'foo bar',
+            'destination-dir' => []
+        ]);
+        $destinationFile = $root . '/destination-dir/some-file';
         $this->config = [
             'files' => [
-                $root->url() . '/some-file' => $destinationFile
+                $root . '/some-file' => $destinationFile
             ]
         ];
 
-        $sut = $this->make_instance();
+        $sut = $this->makeInstance();
 
         $sut->copyFiles();
 
@@ -161,10 +169,10 @@ class CopierTest extends \Codeception\Test\Unit
      * @test
      * it should write source folder to destination folder
      */
-    public function it_should_write_source_folder_to_destination_folder()
+    public function it_should_write_source_folder_to_destination_folder(): void
     {
-        $root = vfsStream::setup('root', 0777, [
-            'folder' => [
+        $root = FS::tmpDir('copier_', [
+            'some-dir' => [
                 'some-file' => 'some file content',
                 'sub-folder' => [
                     'some-sub-file' => 'some sub file content',
@@ -173,18 +181,16 @@ class CopierTest extends \Codeception\Test\Unit
                     ]
                 ]
             ],
-            'destination' => []
+            'destination-dir' => []
         ]);
-        $destFolder = vfsStream::newDirectory('destination', 0777);
-        $root->addChild($destFolder);
-        $destination = $root->url() . '/destination/folder';
+        $destination = $root . '/destination-dir/some-dir';
         $this->config = [
             'files' => [
-                $root->url() . '/folder' => $destination
+                $root . '/some-dir' => $destination
             ]
         ];
 
-        $sut = $this->make_instance();
+        $sut = $this->makeInstance();
 
         $sut->copyFiles();
 
@@ -206,9 +212,9 @@ class CopierTest extends \Codeception\Test\Unit
      * @test
      * it should copy multiple files and structures
      */
-    public function it_should_copy_multiple_files_and_structures()
+    public function it_should_copy_multiple_files_and_structures(): void
     {
-        $root = vfsStream::setup('root', 0777, [
+        $root = FS::tmpDir('copier_', [
             'src-folder' => [
                 'some-file' => 'some file content',
                 'sub-src-folder' => [
@@ -230,20 +236,18 @@ class CopierTest extends \Codeception\Test\Unit
             ],
             'destination' => []
         ]);
-        $destFolder = vfsStream::newDirectory('destination', 0777);
-        $root->addChild($destFolder);
-        $destination = $root->url() . '/destination/src-folder';
-        $destinationTwo = $root->url() . '/destination/file2';
-        $destinationThree = $root->url() . '/destination/folder2';
+        $destinationOne = $root . '/destination/src-folder';
+        $destinationTwo = $root . '/destination/file2';
+        $destinationThree = $root . '/destination/folder2';
         $this->config = [
             'files' => [
-                $root->url() . '/src-folder' => $destination,
-                $root->url() . '/fileTwo' => $destinationTwo,
-                $root->url() . '/src-folder-two' => $destinationThree,
+                $root . '/src-folder' => $destinationOne,
+                $root . '/fileTwo' => $destinationTwo,
+                $root . '/src-folder-two' => $destinationThree,
             ]
         ];
 
-        $sut = $this->make_instance();
+        $sut = $this->makeInstance();
 
         $sut->copyFiles();
 
@@ -251,17 +255,18 @@ class CopierTest extends \Codeception\Test\Unit
         $this->assertEquals('file 2 content', file_get_contents($destinationTwo));
 
 
-        $this->assertFileExists($destination);
-        $this->assertFileExists($destination . '/some-file');
-        $this->assertEquals('some file content', file_get_contents($destination . '/some-file'));
-        $this->assertFileExists($destination . '/sub-src-folder');
-        $this->assertFileExists($destination . '/sub-src-folder/some-sub-file');
-        $this->assertEquals('some sub file content', file_get_contents($destination . '/sub-src-folder/some-sub-file'));
-        $this->assertFileExists($destination . '/sub-src-folder/sub-src-folder-2');
-        $this->assertFileExists($destination . '/sub-src-folder/sub-src-folder-2/some-sub-file-2');
+        $this->assertFileExists($destinationOne);
+        $this->assertFileExists($destinationOne . '/some-file');
+        $this->assertEquals('some file content', file_get_contents($destinationOne . '/some-file'));
+        $this->assertFileExists($destinationOne . '/sub-src-folder');
+        $this->assertFileExists($destinationOne . '/sub-src-folder/some-sub-file');
+        $this->assertEquals('some sub file content',
+            file_get_contents($destinationOne . '/sub-src-folder/some-sub-file'));
+        $this->assertFileExists($destinationOne . '/sub-src-folder/sub-src-folder-2');
+        $this->assertFileExists($destinationOne . '/sub-src-folder/sub-src-folder-2/some-sub-file-2');
         $this->assertEquals(
             'some sub file 2 content',
-            file_get_contents($destination . '/sub-src-folder/sub-src-folder-2/some-sub-file-2')
+            file_get_contents($destinationOne . '/sub-src-folder/sub-src-folder-2/some-sub-file-2')
         );
 
         $this->assertFileExists($destinationThree);
@@ -285,10 +290,12 @@ class CopierTest extends \Codeception\Test\Unit
      * @test
      * it should allow for relative path in source in respect to cwd
      */
-    public function it_should_allow_for_relative_path_in_source_in_respect_to_cwd()
+    public function it_should_allow_for_relative_path_in_source_in_respect_to_cwd(): void
     {
-        $root = vfsStream::setup('root', 0777, ['destination' => []]);
-        $destination = $root->url() . '/destination';
+        $root = FS::tmpDir('copier_', [
+            'destination' => []
+        ]);
+        $destination = $root . '/destination';
         $this->config = [
             'files' => [
                 'tests/_data/some-file' => $destination . '/some-file',
@@ -296,7 +303,7 @@ class CopierTest extends \Codeception\Test\Unit
             ]
         ];
 
-        $sut = $this->make_instance();
+        $sut = $this->makeInstance();
 
         $sut->copyFiles();
 
@@ -309,32 +316,31 @@ class CopierTest extends \Codeception\Test\Unit
      * @test
      * it should allow for relative destinations
      */
-    public function it_should_allow_for_relative_destinations()
+    public function it_should_allow_for_relative_destinations(): void
     {
-        mkdir(codecept_data_dir('destination'), 0777, true);
+        $root = FS::tmpDir('copier_', [
+            'some-file' => 'some file content',
+            'some-folder' => [
+                'some-file' => 'some other file content'
+            ],
+            'destination' => []
+        ]);
+        $rootRelativePath = FS::relativePath(getcwd(), $root);
 
         $this->config = [
             'files' => [
-                codecept_data_dir('some-file') => 'tests/_data/destination/some-file',
-                codecept_data_dir('some-folder') => 'tests/_data/destination/some-folder',
+                $root . '/some-file' => $rootRelativePath . '/destination/some-file',
+                $root . '/some-folder' => $rootRelativePath . '/destination/some-folder',
             ]
         ];
 
-        $sut = $this->make_instance();
+        $sut = $this->makeInstance();
 
         $sut->copyFiles();
 
-        $destination = codecept_data_dir('destination');
+        $destination = $root . '/destination';
         $this->assertFileExists($destination . '/some-file');
         $this->assertFileExists($destination . '/some-folder');
         $this->assertFileExists($destination . '/some-folder/some-file');
-    }
-
-    protected function _after()
-    {
-        $destination = codecept_data_dir('destination');
-        if (file_exists($destination)) {
-            FS::rrmdir($destination);
-        }
     }
 }
