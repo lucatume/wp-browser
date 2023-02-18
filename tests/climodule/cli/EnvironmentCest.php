9dd5d0b0b98d576fcb11d5412d3c92427b5905a8
Luca Tumedei <luca@theaveragedev.com>
2023-02-18 11:40:30 +0100

    test(climodule) update test case

--- tests/climodule/cli/EnvironmentCest.php
+++ tests/climodule/cli/EnvironmentCest.php
@@ -1,301 +1,263 @@
 <?php
+
 namespace cli;
 
 use ClimoduleTester;
 
+use tad\WPBrowser\Adapters\PHPUnit\Framework\Assert;
 use function putenv;
 
 class EnvironmentCest
 {
-    
-    /**
-     * @test
-     */
-    public function that_environment_is_inherited_from_putenv(ClimoduleTester $I)
+
+    public function test_that_environment_is_inherited_from_putenv(ClimoduleTester $I)
     {
         putenv('BAZ=BIZ');
-    
+
         try {
-            $I->cli(['eval','"var_dump(\$_SERVER[\'BAZ\'] ?? \'Not set\');"']);
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'BAZ\'] ?? \'Not set\');"']);
             $I->seeInShellOutput('BIZ');
         } finally {
             putenv('BAZ');
         }
 
     }
-    
-    /**
-     * @test
-     */
-    public function that_environment_is_inherited_from_ENV(ClimoduleTester $I)
+
+    public function test_that_environment_is_inherited_from_ENV(ClimoduleTester $I)
     {
+        if (PHP_VERSION_ID < 70100) {
+            Assert::markTestSkipped('This test should only run on PHP >= 7.1');
+        }
+
         $_ENV['FOO'] = 'BAR';
-    
+
         try {
-            $I->cli(['eval','"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"']);
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"']);
             $I->seeInShellOutput('BAR');
         } finally {
             unset($_ENV['FOO']);
         }
     }
-    
-    /**
-     * @test
-     */
-    public function inheriting_env_on_php_greater_than_71000(ClimoduleTester $I)
+
+    public function test_inheriting_env(ClimoduleTester $I)
     {
-        if(PHP_VERSION_ID <= 70100) {
-            $I->markTestSkipped('This test should only run on PHP >= 7.1');
+        if (PHP_VERSION_ID <= 70100) {
+            Assert::markTestSkipped('This test should only run on PHP >= 7.1');
             return;
         }
-    
+
         try {
             $_ENV['X_FOO'] = 'X_BAR';
             putenv('X_BAZ=X_BIZ');
-    
-            $I->cli(['eval','"var_dump(\$_SERVER[\'X_FOO\'] ?? \'Not set\');"']);
+
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'X_FOO\'] ?? \'Not set\');"']);
             $I->seeInShellOutput('X_BAR');
-    
-            $I->cli(['eval','"var_dump(\$_SERVER[\'X_BAZ\'] ?? \'Not set\');"']);
+
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'X_BAZ\'] ?? \'Not set\');"']);
             $I->seeInShellOutput('X_BIZ');
-    
+
             // putenv has a higher priority.
             putenv('X_FOO=X_BAR_putenv');
-    
-            $I->cli(['eval','"var_dump(\$_SERVER[\'X_FOO\'] ?? \'Not set\');"']);
+
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'X_FOO\'] ?? \'Not set\');"']);
             $I->seeInShellOutput('X_BAR_putenv');
-        }finally {
+        } finally {
             unset($_ENV['X_FOO']);
             putenv('X_BAZ');
             putenv('X_FOO');
         }
 
     }
-    
-    /**
-     * @test
-     */
-    public function inheriting_env_on_php_less_than_71000(ClimoduleTester $I)
+
+    public function test_that_wp_cli_config_variables_dont_prevent_inheriting_the_environment(ClimoduleTester $I)
     {
-        if(PHP_VERSION_ID > 70100) {
-            $I->markTestSkipped('This test should only run on PHP < 7.1');
+        if (PHP_VERSION_ID <= 70100) {
+            Assert::markTestSkipped('This test should only run on PHP >= 7.1');
             return;
         }
-        
-        // putenv won't work on PHP < 7.1, and it never did either.
-        $_ENV['X_FOO'] = 'X_BAR';
-    
-        try {
-            $I->cli(['eval','"var_dump(\$_SERVER[\'X_FOO\'] ?? \'Not set\');"']);
-            $I->seeInShellOutput('X_BAR');
-        }finally {
-            unset($_ENV['X_FOO']);
-        }
 
-    }
-    
-    /**
-     * @test
-     */
-    public function that_wp_cli_config_variables_dont_prevent_inheriting_the_environment(ClimoduleTester $I)
-    {
         // This will set a custom env variable in WPCLI::buildProcessEnv()
         // which will be inherited by the child process.
         // This will cause proc_open to not receive to current env by default.
-        $I->setCliEnv('disable-auto-check-update','1');
-    
+        $I->setCliEnv('disable-auto-check-update', '1');
+
         putenv('X_BOO=BAM');
-    
+
         try {
-            $I->cli(['eval','"var_dump(\$_SERVER[\'X_BOO\'] ?? \'Not set\');"']);
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'X_BOO\'] ?? \'Not set\');"']);
             $I->seeInShellOutput('BAM');
-    
-            $I->cli(['eval','"var_dump(\$_SERVER[\'WP_CLI_DISABLE_AUTO_CHECK_UPDATE\'] ?? \'Not set\');"']);
+
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'WP_CLI_DISABLE_AUTO_CHECK_UPDATE\'] ?? \'Not set\');"']);
             $I->seeInShellOutput('1');
-        }finally {
+        } finally {
             putenv('X_BOO');
         }
 
     }
-    
-    /**
-     * @test
-     */
-    public function that_per_process_environment_variables_can_be_set(ClimoduleTester $I)
+
+    public function test_that_per_process_environment_variables_can_be_set(ClimoduleTester $I)
     {
-        $I->cli(['eval','"var_dump(\$_SERVER[\'BAZ\'] ?? \'Not set\');"'], ['BAZ' => 'BIZ']);
+        $I->cli(['eval', '"var_dump(\$_SERVER[\'BAZ\'] ?? \'Not set\');"'], ['BAZ' => 'BIZ']);
         $I->seeInShellOutput('BIZ');
-        
+
         putenv('BAZ=global_BIZ');
-    
+
         try {
             // per process has priority.
-            $I->cli(['eval','"var_dump(\$_SERVER[\'BAZ\'] ?? \'Not set\');"'], ['BAZ' => 'BIZ']);
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'BAZ\'] ?? \'Not set\');"'], ['BAZ' => 'BIZ']);
             $I->seeInShellOutput('BIZ');
-        }finally {
+        } finally {
             putenv('BAZ');
         }
 
     }
-    
-    /**
-     * @test
-     */
-    public function that_global_env_variables_can_be_set_in_the_module(ClimoduleTester $I)
+
+    public function test_that_global_env_variables_can_be_set_in_the_module(ClimoduleTester $I)
     {
         $I->haveInShellEnvironment(['FOO' => 'BAR']);
-        
-        $I->cli(['eval','"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"']);
+
+        $I->cli(['eval', '"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"']);
         $I->seeInShellOutput('BAR');
-        
+
         $I->haveInShellEnvironment(['BAZ' => 'BIZ']);
         // Present for all commands
-        $I->cli(['eval','"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"']);
+        $I->cli(['eval', '"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"']);
         $I->seeInShellOutput('BAR');
         // Merged between calls
-        $I->cli(['eval','"var_dump(\$_SERVER[\'BAZ\'] ?? \'Not set\');"']);
+        $I->cli(['eval', '"var_dump(\$_SERVER[\'BAZ\'] ?? \'Not set\');"']);
         $I->seeInShellOutput('BIZ');
-        
+
         putenv('FOO=global_BAR');
-    
+
         try {
-            $I->cli(['eval','"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"']);
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"']);
             $I->seeInShellOutput('BAR');
-    
-            $I->cli(['eval','"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"'], ['FOO' => 'BAR_PROCESS']);
+
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"'], ['FOO' => 'BAR_PROCESS']);
             $I->seeInShellOutput('BAR_PROCESS');
-        }finally {
+        } finally {
             putenv('FOO');
         }
-        
+
     }
-    
-    /**
-     * @test
-     */
-    public function that_global_env_variables_can_be_passed_from_the_config_file(ClimoduleTester $I)
+
+    public function test_that_global_env_variables_can_be_passed_from_the_config_file(ClimoduleTester $I)
     {
         // This emulates having a FOO key under the env config.
         $I->setCliEnv('FOO', 'BAR');
-        $I->setCliEnv('disable-auto-check-update','1');
-        
-        $I->cli(['eval','"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"']);
+        $I->setCliEnv('disable-auto-check-update', '1');
+
+        $I->cli(['eval', '"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"']);
         $I->seeInShellOutput('BAR');
-        
-        $I->cli(['eval','"var_dump(\$_SERVER[\'disable-auto-check-update\'] ?? \'Not set\');"']);
+
+        $I->cli(['eval', '"var_dump(\$_SERVER[\'disable-auto-check-update\'] ?? \'Not set\');"']);
         $I->seeInShellOutput('Not set');
-    
-        $I->cli(['eval','"var_dump(\$_SERVER[\'WP_CLI_DISABLE_AUTO_CHECK_UPDATE\'] ?? \'Not set\');"']);
+
+        $I->cli(['eval', '"var_dump(\$_SERVER[\'WP_CLI_DISABLE_AUTO_CHECK_UPDATE\'] ?? \'Not set\');"']);
         $I->seeInShellOutput('1');
-        
+
         putenv('FOO=global_BAR');
-        
+
         try {
             // per process putenv() DOES NOT HAVE priority. Use $I->haveInShellEnvironment() for that.
-            $I->cli(['eval','"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"']);
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"']);
             $I->seeInShellOutput('BAR');
-    
+
             putenv('FOO');
-            
+
             $I->haveInShellEnvironment(['FOO' => 'BAR_SHELL_ENV']);
-    
+
             // per process global env variables still have priority.
-            $I->cli(['eval','"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"']);
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"']);
             $I->seeInShellOutput('BAR_SHELL_ENV');
-            
+
             // per process env variables still have priority.
-            $I->cli(['eval','"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"'], ['FOO' => 'BAR_PROCESS']);
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"'], ['FOO' => 'BAR_PROCESS']);
             $I->seeInShellOutput('BAR_PROCESS');
-        }finally {
+        } finally {
             putenv('FOO');
         }
     }
-    
-    /**
-     * @test
-     */
-    public function that_inheriting_anything_from_the_global_environment_can_be_prevented(ClimoduleTester $I)
+
+    public function test_that_inheriting_anything_from_the_global_environment_can_be_prevented(ClimoduleTester $I)
     {
         putenv('FOO=global_BAR');
-    
+
         try {
-            
-            $I->cli(['eval','"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"'], [], false);
+
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"'], [], false);
             $I->seeInShellOutput('Not set');
-            
+
             // This still works.
             $I->haveInShellEnvironment(['FOO' => 'BAR']);
-            $I->cli(['eval','"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"'], [], false);
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"'], [], false);
             $I->seeInShellOutput('BAR');
-            
+
             // WP_BROWSER_HOST request marker is still set.
-            $I->cli(['eval','"var_dump(\$_SERVER[\'WPBROWSER_HOST_REQUEST\'] ?? \'Not set\');"'], [], false);
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'WPBROWSER_HOST_REQUEST\'] ?? \'Not set\');"'], [], false);
             $I->seeInShellOutput('1');
-            
-        }finally {
+
+        } finally {
             putenv('FOO');
         }
     }
-    
-    /**
-     * @test
-     */
-    public function that_inheriting_some_global_environment_variables_can_be_prevented(ClimoduleTester $I)
+
+    public function test_that_inheriting_some_global_environment_variables_can_be_prevented(ClimoduleTester $I)
     {
-        if(PHP_VERSION_ID < 70100) {
-            $I->markTestSkipped('Blocking some global env vars is not possible on PHP < 7.1');
+        if (PHP_VERSION_ID < 70100) {
+            Assert::markTestSkipped('Blocking some global env vars is not possible on PHP < 7.1');
             return;
         }
-        
+
         putenv('FOO=global_FOO');
         putenv('BAR=global_BAR');
         try {
-            
+
             $I->dontInheritShellEnvironment(['FOO']);
-            
-            $I->cli(['eval','"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"']);
+
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"']);
             $I->seeInShellOutput('Not set');
-    
+
             // Also blocked when passing explicit env
-            $I->cli(['eval','"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"'], ['SOMETHING' => 'WHATEVER']);
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'FOO\'] ?? \'Not set\');"'], ['SOMETHING' => 'WHATEVER']);
             $I->seeInShellOutput('Not set');
-    
-            $I->cli(['eval','"var_dump(\$_SERVER[\'BAR\'] ?? \'Not set\');"']);
+
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'BAR\'] ?? \'Not set\');"']);
             $I->seeInShellOutput('global_BAR');
-            
-        }finally {
+
+        } finally {
             putenv('FOO');
             putenv('BAR');
         }
     }
-    
-    /**
-     * @test
-     */
-    public function that_inheriting_some_global_environment_variables_can_be_prevented_from_the_configuration(ClimoduleTester $I)
-    {
-        if(PHP_VERSION_ID < 70100) {
-            $I->markTestSkipped('Blocking some global env vars is not possible on PHP < 7.1');
+
+    public function test_that_inheriting_some_global_environment_variables_can_be_prevented_from_the_configuration(
+        ClimoduleTester $I
+    ) {
+        if (PHP_VERSION_ID < 70100) {
+            Assert::markTestSkipped('Blocking some global env vars is not possible on PHP < 7.1');
             return;
         }
-        
+
         putenv('FOO_BLOCKED=global_FOO');
         try {
-            
-            $I->cli(['eval','"var_dump(\$_SERVER[\'FOO_BLOCKED\'] ?? \'Not set\');"']);
+
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'FOO_BLOCKED\'] ?? \'Not set\');"']);
             $I->seeInShellOutput('Not set');
-    
-            $I->cli(['eval','"var_dump(\$_SERVER[\'FOO_BLOCKED\'] ?? \'Not set\');"'], ['FOO_BLOCKED'=> 'THIS_WORKS']);
+
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'FOO_BLOCKED\'] ?? \'Not set\');"'],
+                ['FOO_BLOCKED' => 'THIS_WORKS']);
             $I->seeInShellOutput('THIS_WORKS');
-            
+
             $I->haveInShellEnvironment(['FOO_BLOCKED', 'THIS_WORKS_TOO']);
-    
-            $I->cli(['eval','"var_dump(\$_SERVER[\'FOO_BLOCKED\'] ?? \'Not set\');"'], ['FOO_BLOCKED'=> 'THIS_WORKS_TOO']);
+
+            $I->cli(['eval', '"var_dump(\$_SERVER[\'FOO_BLOCKED\'] ?? \'Not set\');"'],
+                ['FOO_BLOCKED' => 'THIS_WORKS_TOO']);
             $I->seeInShellOutput('THIS_WORKS_TOO');
-            
-        }finally {
+
+        } finally {
             putenv('FOO_BLOCKED');
         }
     }
-    
+
 }
