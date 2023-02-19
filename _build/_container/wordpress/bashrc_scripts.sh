9f6980ece6ff8df9eed464bb54d974dd043b480c
Luca Tumedei <luca@theaveragedev.com>
2023-02-19 15:21:36 +0100

    build(_container/wordpress) disable uopz by default, enable in cli context

--- _build/_container/wordpress/bashrc_scripts.sh
+++ _build/_container/wordpress/bashrc_scripts.sh
@@ -1,4 +1,4 @@
-alias c="vendor/bin/codecept"
-alias cr="vendor/bin/codecept run"
+alias c="php -dextension=uopz.so vendor/bin/codecept"
+alias cr="php -dextension=uopz.so vendor/bin/codecept run"
 alias xon="xdebug-on"
 alias xoff="xdebug-off"
