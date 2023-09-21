7a33f8264f4b5e86c585de2f87c99681294da2c6
Luca Tumedei <luca@theaveragedev.com>
2023-09-21 17:02:55 +0200

    test(acceptance) add AJAX page ops coverage

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -1,26 +1,27 @@
 <?php
 
+use PHPUnit\Framework\Assert;
+
 
 /**
  * Inherited Methods
- * @method void wantToTest($text)
- * @method void wantTo($text)
- * @method void execute($callable)
- * @method void expectTo($prediction)
- * @method void expect($prediction)
- * @method void amGoingTo($argumentation)
- * @method void am($role)
- * @method void lookForwardTo($achieveValue)
- * @method void comment($description)
- * @method \Codeception\Lib\Friend haveFriend($name, $actorClass = null)
+ * @method void wantToTest( $text )
+ * @method void wantTo( $text )
+ * @method void execute( $callable )
+ * @method void expectTo( $prediction )
+ * @method void expect( $prediction )
+ * @method void amGoingTo( $argumentation )
+ * @method void am( $role )
+ * @method void lookForwardTo( $achieveValue )
+ * @method void comment( $description )
+ * @method \Codeception\Lib\Friend haveFriend( $name, $actorClass = null )
  *
  * @SuppressWarnings(PHPMD)
  */
-class AcceptanceTester extends \Codeception\Actor
-{
-    use _generated\AcceptanceTesterActions;
+class AcceptanceTester extends \Codeception\Actor {
+	use _generated\AcceptanceTesterActions;
 
-    /**
-     * Define custom actions here
-     */
+	/**
+	 * Define custom actions here
+	 */
 }
