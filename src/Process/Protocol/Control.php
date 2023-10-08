4f054a0bb45f6abfa8564a2d5345e5faea4b472b
Brian Henry <BrianHenryIE@gmail.com>
2023-10-07 18:43:31 -0700

    Check `vendor/autoload.php` exists.

--- src/Process/Protocol/Control.php
+++ src/Process/Protocol/Control.php
@@ -84,7 +84,10 @@ class Control
             'cwd' => getcwd() ?: codecept_root_dir(),
             'codeceptionRootDir' => codecept_root_dir(),
             'codeceptionConfig' => Configuration::isEmpty() ? [] : Configuration::config(),
-            'composerAutoloadPath' => $GLOBALS['_composer_autoload_path'] ?? getcwd() . '/vendor/autoload.php',
+            'composerAutoloadPath' => $GLOBALS['_composer_autoload_path']
+                                      ?? is_readable(getcwd() . '/vendor/autoload.php')
+                                            ? getcwd() . '/vendor/autoload.php'
+                                            : null,
             'composerBinDir' => $GLOBALS['_composer_bin_dir'] ?? null,
             'env' => $_ENV,
         ];
