879460b3074c424e0459eb281b91fdf55c613d57
Luca Tumedei <luca@theaveragedev.com>
2023-11-22 09:47:27 +0100

    test(unit) update 3.5 snapshots

--- tests/unit/lucatume/WPBrowser/WordPress/__snapshots__/WpConfigFileGeneratorTest__3.5__should_correctly_produce_a_wp_config_php_file_with_custom_constants__0.snapshot.php
+++ tests/unit/lucatume/WPBrowser/WordPress/__snapshots__/WpConfigFileGeneratorTest__3.5__should_correctly_produce_a_wp_config_php_file_with_custom_constants__0.snapshot.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'root' );
 define( 'DB_PASSWORD', 'password' );
 
 /** Database hostname */
-define( 'DB_HOST', '127.0.0.1:3591' );
+define( 'DB_HOST', '127.0.0.1:2391' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
