c3be09bb439f668523b8a62a3cd8852ca7fc10a9
Luca Tumedei <luca@theaveragedev.com>
2023-11-22 08:57:07 +0100

    build(config/containers) remove directory

--- config/containers/php/xdebug-off.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-xdebug_config_file="$(php --ini | grep xdebug | cut -d, -f1)"
-sed -i '/^zend_extension/ s/zend_extension/;zend_extension/g' "$xdebug_config_file"
-# Kill the oldest php-fpm process, the manager.
-pkill -o -USR2 php-fpm
-# Restart the php-fpm server and php-fpm when used as a module, ignore errors if not running.
-/etc/init.d/apache2 reload >/dev/null 2>&1
-php -v
