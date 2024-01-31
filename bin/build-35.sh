ad52749d746868b6b2a9e6e02e4cce43b28394f9
Luca Tumedei <luca@theaveragedev.com>
2024-01-31 09:49:57 +0100

    build(bin/build-35.sh) allow building just one file

--- bin/build-35.sh
+++ bin/build-35.sh
@@ -2,6 +2,8 @@
 
 set -eu
 
+# Accept an optional set of paths.
+files=$@
 [ -d ./.build/35 ] && rm -rf ./.build/35
 mkdir -p ./.build/35
 rsync -av --exclude-from=bin/build-35-exclusions.txt . ./.build/35
@@ -9,7 +11,11 @@ cd ./.build/35
 rm -rf vendor composer.lock
 composer require --dev rector/rector:0.19.2 -W
 composer dump-autoload
-vendor/bin/rector --config=config/rector-35.php
+if [ -z "$files" ]; then
+    vendor/bin/rector process src --config=config/rector-35.php
+else
+    vendor/bin/rector process --config=config/rector-35.php $files
+fi
 rm -rf vendor composer.lock composer.json
 cp config/composer-35.json composer.json
 cd -
