afd58a44c206228438fecec51158888624b0ad13
Luca Tumedei <luca@theaveragedev.com>
2024-02-18 17:22:48 +0100

    build(rector/rector) update to 0.19.8

--- bin/build-35.sh
+++ bin/build-35.sh
@@ -9,7 +9,7 @@ mkdir -p ./.build/35
 rsync -av --exclude-from=bin/build-35-exclusions.txt . ./.build/35
 cd ./.build/35
 rm -rf vendor composer.lock
-composer require --dev rector/rector:0.19.2 -W
+composer require --dev rector/rector:0.19.8 -W
 composer dump-autoload
 if [ -z "$files" ]; then
     vendor/bin/rector process src --config=config/rector-35.php
@@ -18,4 +18,5 @@ else
 fi
 rm -rf vendor composer.lock composer.json
 cp config/composer-35.json composer.json
+rm -rf config
 cd -
